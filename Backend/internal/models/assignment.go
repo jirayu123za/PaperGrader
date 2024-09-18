badb1f300bdebac5e2807fb86e9d66c75eeac4aa
jirayu <jirayu004za@gmail.com>
2024-09-18 22:23:58 +0700

    update models

--- Backend/internal/models/assignment.go
+++ Backend/internal/models/assignment.go
@@ -3,18 +3,26 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Assignment struct {
-	AssignmentID          string           `gorm:"primaryKey;autoIncrement"`
-	CourseID              string           `gorm:"not null"`
-	AssignmentName        string           `gorm:"type:varchar(255);not null"`
-	AssignmentDescription string           `gorm:"type:varchar(255)"`
-	DueDate               time.Time        `gorm:"not null"`
+	AssignmentID          uuid.UUID        `gorm:"primaryKey"`
+	CourseID              uuid.UUID        `gorm:"not null" json:"course_id"`
+	AssignmentName        string           `gorm:"type:varchar(255);not null" json:"assignment_name"`
+	AssignmentDescription string           `gorm:"type:varchar(255)" json:"assignment_description"`
+	DueDate               time.Time        `gorm:"not null" json:"due_date"`
 	AssignmentFiles       []AssignmentFile `gorm:"foreignKey:AssignmentID"`
 	Submissions           []Submission     `gorm:"foreignKey:AssignmentID"`
 	CreatedAt             time.Time
 	UpdatedAt             time.Time
 	DeletedAt             gorm.DeletedAt `gorm:"index"`
+}
+
+func (assignment *Assignment) BeforeCreate(tx *gorm.DB) (err error) {
+	if assignment.AssignmentID == uuid.Nil {
+		assignment.AssignmentID = uuid.New()
+	}
+	return
 }
--- Backend/internal/models/instructor_list.go
+++ Backend/internal/models/instructor_list.go
@@ -8,10 +8,17 @@ import (
 )
 
 type InstructorList struct {
-	ListID    int       `gorm:"primaryKey;autoIncrement"`
-	UserID    uuid.UUID `gorm:"not null"`
-	CourseID  string    `gorm:"not null"`
+	ListID    uuid.UUID `gorm:"primaryKey" json:"list_id"`
+	UserID    uuid.UUID `gorm:"not null" json:"user_id"`
+	CourseID  uuid.UUID `gorm:"not null" json:"course_id"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (instructorList *InstructorList) BeforeCreate(tx *gorm.DB) (err error) {
+	if instructorList.ListID == uuid.Nil {
+		instructorList.ListID = uuid.New()
+	}
+	return
 }
