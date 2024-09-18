65475e768e4f680cf83b3bd89ad208f24463ce6c
jirayu <jirayu004za@gmail.com>
2024-09-18 20:23:13 +0700

    config minio connection

--- Backend/internal/storage/storage.go
+++ Backend/internal/storage/storage.go
@@ -9,39 +9,6 @@ import (
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
-/*
-var Store *minio.Storage
-
-func InitMinIO() {
-	config.LoadEnv()
-	bucketName := os.Getenv("MINIO_BUCKET_NAME")
-	endpoint := os.Getenv("MINIO_ENDPOINT")
-	accessKey := os.Getenv("MINIO_ACCESS_KEY")
-	secretKey := os.Getenv("MINIO_SECRET_KEY")
-
-	Store = minio.New(minio.Config{
-		Bucket:   bucketName,
-		Endpoint: endpoint,
-		Credentials: minio.Credentials{
-			AccessKeyID:     accessKey,
-			SecretAccessKey: secretKey,
-		},
-		Secure: false,
-	})
-
-	err := Store.CheckBucket()
-	if err != nil {
-		fmt.Println("Bucket doesn't exist, creating bucket...")
-
-		err = Store.CreateBucket()
-		if err != nil {
-			log.Fatalf("Failed to create bucket: %v", err)
-		}
-		fmt.Println("Bucket created successfully.")
-	}
-}
-*/
-
 func MinioConnection() (*minio.Client, error) {
 	ctx := context.Background()
 	endpoint := os.Getenv("MINIO_ENDPOINT")
@@ -63,7 +30,6 @@ func MinioConnection() (*minio.Client, error) {
 
 	err := minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
 	if err != nil {
-		// Check to see if we already own this bucket (which happens if you run this twice)
 		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
 		if errBucketExists == nil && exists {
 			log.Printf("We already own %s\n", bucketName)
