d405dc318667341c74af511f96d22ef6014fb7e7
jirayu <jirayu004za@gmail.com>
2024-09-18 22:43:58 +0700

    migrate tb University

--- Backend/internal/database/database.go
+++ Backend/internal/database/database.go
@@ -46,7 +46,8 @@ func ConnectPostgres(migrate bool) *gorm.DB {
 			&models.Enrollment{},
 			&models.InstructorList{},
 			&models.Submission{},
-			&models.Upload{})
+			&models.Upload{},
+			&models.University{})
 		if err != nil {
 			log.Fatal("Failed to migrate database: ", err)
 		}
