dd898ab2ca057d96c6d1a3147a0a4dd033d98030
jirayu <jirayu004za@gmail.com>
2024-09-25 17:55:15 +0700

    update with university route

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -51,6 +51,10 @@ func main() {
 	oauthService := services.NewOAuthService(oauthRepo)
 	oauthHandler := adapters.NewHttpOAuthHandler(oauthService, userService)
 
+	universityRepo := adapters.NewGormUniversityRepository(db)
+	universityService := services.NewUniversityService(universityRepo)
+	universityHandler := adapters.NewHttpUniversityHandler(universityService)
+
 	// Routes
 	api := app.Group("/")
 	apiGroup := api.Group("/api")
@@ -60,14 +64,22 @@ func main() {
 			"error":   nil,
 		})
 	})
+
 	apiGroup.Post("/file", fileHandler.CreateFile)
-	apiGroup.Get("/file/:name", fileHandler.GetFileByID)
+	apiGroup.Get("/file", fileHandler.GetFileByID)
+	apiGroup.Get("/file/url", fileHandler.GetFileURL)
 
 	apiGroup.Get("/google", oauthHandler.GetGoogleLoginURL)
 	apiGroup.Get("/google/callback", oauthHandler.GetGoogleCallBack)
 
 	apiGroup.Post("/user", userHandler.CreateUser)
 	apiGroup.Get("/user/:googleID", userHandler.GetUserByID)
+
+	apiGroup.Post("/university", universityHandler.CreateUniversity)
+	apiGroup.Get("/universities", universityHandler.GetUniversities)
+	apiGroup.Get("/university", universityHandler.GetUniversityByID)
+	apiGroup.Put("/university", universityHandler.UpdateUniversity)
+	apiGroup.Delete("/university", universityHandler.DeleteUniversity)
 
 	if err := app.Listen(":" + port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
