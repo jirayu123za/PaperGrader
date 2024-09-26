aa3d00cd4eed9407de26bdaf5554d95272dddec0
pulom999 <pulom999@gmail.com>
2024-09-27 01:19:48 +0700

    Update FileStore.ts

--- Frontend/store/FileStore.ts
+++ Frontend/store/FileStore.ts
@@ -1,11 +1,25 @@
 import create from 'zustand';
 
-interface FileState {
-  file: File | null;
-  setFile: (file: File) => void;
+interface FileStoreState {
+  files: File[];
+  setFiles: (files: File[]) => void;
+  addFile: (file: File) => void;
+  removeFile: (fileName: string) => void;
+  templateFile: File | null; // เพิ่ม templateFile
+  setTemplateFile: (file: File | null) => void; // เพิ่ม setTemplateFile
 }
 
-export const useFileStore = create<FileState>((set) => ({
-  file: null,
-  setFile: (file) => set({ file }),
+export const useFileStore = create<FileStoreState>((set) => ({
+  files: [],
+  setFiles: (files) => set({ files }),
+  addFile: (file) =>
+    set((state) => ({
+      files: [...state.files, file],
+    })),
+  removeFile: (fileName) =>
+    set((state) => ({
+      files: state.files.filter((file) => file.name !== fileName),
+    })),
+  templateFile: null, // กำหนดค่าเริ่มต้นให้ templateFile
+  setTemplateFile: (file) => set({ templateFile: file }), // ฟังก์ชันสำหรับตั้งค่า templateFile
 }));
