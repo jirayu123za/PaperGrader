8facb5d89075efa76078d4d86759c48a45af6240
jirayu <jirayu004za@gmail.com>
2024-10-13 01:08:07 +0700

    update json and rename SubmissionFileID to SubmissionFileName

--- Backend/internal/models/submission.go
+++ Backend/internal/models/submission.go
@@ -8,14 +8,14 @@ import (
 )
 
 type Submission struct {
-	SubmissionID     uuid.UUID `gorm:"primaryKey"`
-	UserID           uuid.UUID `gorm:"not null"`
-	AssignmentID     uuid.UUID `gorm:"not null"`
-	SubmissionFileID uuid.UUID `gorm:"not null"`
-	SubmittedAt      time.Time `gorm:"not null"`
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
-	DeletedAt        gorm.DeletedAt `gorm:"index"`
+	SubmissionID       uuid.UUID `gorm:"primaryKey"`
+	UserID             uuid.UUID `gorm:"not null" json:"user_id"`
+	AssignmentID       uuid.UUID `gorm:"not null" json:"assignment_id"`
+	SubmissionFileName uuid.UUID `gorm:"not null" json:"submission_file_name"`
+	SubmittedAt        time.Time `gorm:"not null" json:"submitted_at"`
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+	DeletedAt          gorm.DeletedAt `gorm:"index"`
 }
 
 func (submission *Submission) BeforeCreate(tx *gorm.DB) (err error) {
