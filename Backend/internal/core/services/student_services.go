e5164aba763cc8a8b47ec96679db9750e95f6b36
jirayu <jirayu004za@gmail.com>
2024-10-15 03:09:08 +0700

    impl GetAssignmentNamesWithCourseIDAndAssignmentID, GetPDFFileNamesAndURLs services

--- Backend/internal/core/services/student_services.go
+++ Backend/internal/core/services/student_services.go
@@ -9,16 +9,20 @@ import (
 // Primary port
 type StudentService interface {
 	GetCoursesAndAssignments(UserID uuid.UUID) ([]map[string]interface{}, error)
+	GetAssignmentNamesWithCourseIDAndAssignmentID(CourseID uuid.UUID, AssignmentID uuid.UUID) (fileNames []string, err error)
+	GetPDFFileNamesAndURLs(CourseID uuid.UUID, AssignmentID uuid.UUID) (fileNames []string, fileURLs []string, err error)
 }
 
 type StudentServiceImpl struct {
-	repo repositories.StudentRepository
+	repo      repositories.StudentRepository
+	minioRepo repositories.MinIORepository
 }
 
 // func instance business logic call
-func NewStudentService(repo repositories.StudentRepository) StudentService {
+func NewStudentService(repo repositories.StudentRepository, minioRepo repositories.MinIORepository) StudentService {
 	return &StudentServiceImpl{
-		repo: repo,
+		repo:      repo,
+		minioRepo: minioRepo,
 	}
 }
 
@@ -28,4 +32,28 @@ func (s *StudentServiceImpl) GetCoursesAndAssignments(UserID uuid.UUID) ([]map[s
 		return nil, err
 	}
 	return courses, nil
+}
+
+func (s *StudentServiceImpl) GetAssignmentNamesWithCourseIDAndAssignmentID(CourseID uuid.UUID, AssignmentID uuid.UUID) (fileNames []string, err error) {
+	fileNames, err = s.repo.FindAssignmentNamesWithCourseIDAndAssignmentID(CourseID, AssignmentID)
+	if err != nil {
+		return nil, err
+	}
+	return fileNames, nil
+}
+
+func (s *StudentServiceImpl) GetPDFFileNamesAndURLs(CourseID uuid.UUID, AssignmentID uuid.UUID) (fileNames []string, fileURLs []string, err error) {
+	PDFileNames, err := s.repo.FindAssignmentNamesWithCourseIDAndAssignmentID(CourseID, AssignmentID)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	courseIDSlice := []string{CourseID.String()}
+	assignmentIDSlice := []string{AssignmentID.String()}
+
+	returnFileURLs, returnFileNames, err := s.minioRepo.FindFilesAndNames(courseIDSlice, assignmentIDSlice, PDFileNames)
+	if err != nil {
+		return nil, nil, err
+	}
+	return returnFileURLs, returnFileNames, nil
 }
