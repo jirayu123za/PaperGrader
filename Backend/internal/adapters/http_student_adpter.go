e4b9bfed070e2559563fad82612d803a35a8abc5
jirayu <jirayu004za@gmail.com>
2024-10-15 03:09:37 +0700

    impl http handler GetAssignmentNamesWithCourseIDAndAssignmentID, GetPDFFileNamesAndURLs

--- Backend/internal/adapters/http_student_adpter.go
+++ Backend/internal/adapters/http_student_adpter.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 // Primary adapters
@@ -48,5 +49,72 @@ func (h *HttpStudentHandler) GetCoursesAndAssignments(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"message":     "Successfully fetched courses and assignments",
 		"assignments": result,
+	})
+}
+
+func (h *HttpStudentHandler) GetAssignmentNamesWithCourseIDAndAssignmentID(c *fiber.Ctx) error {
+	courseIDParam := c.Query("course_id")
+	courseID, err := uuid.Parse(courseIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid course_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	fileNames, err := h.services.GetAssignmentNamesWithCourseIDAndAssignmentID(courseID, assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get assignment names",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Successfully fetched assignment names",
+		"files":   fileNames,
+	})
+}
+
+func (h *HttpStudentHandler) GetPDFFileNamesAndURLs(c *fiber.Ctx) error {
+	courseIDParam := c.Query("course_id")
+	courseID, err := uuid.Parse(courseIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid course_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	fileNames, urls, err := h.services.GetPDFFileNamesAndURLs(courseID, assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get PDF file names and URLs",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Successfully fetched PDF file names and URLs",
+		"files":   fileNames,
+		"urls":    urls,
 	})
 }
