28d53331c94b6a25bf674c5752fc185871a8f032
jirayu <jirayu004za@gmail.com>
2024-10-15 03:07:20 +0700

    impl repo FindAssignmentNamesWithCourseIDAndAssignmentID

--- Backend/internal/core/repositories/student_repository.go
+++ Backend/internal/core/repositories/student_repository.go
@@ -5,4 +5,5 @@ import "github.com/google/uuid"
 type StudentRepository interface {
 	// find all courses and assignments for a student
 	FindCoursesAndAssignments(UserID uuid.UUID) ([]map[string]interface{}, error)
+	FindAssignmentNamesWithCourseIDAndAssignmentID(CourseID uuid.UUID, AssignmentID uuid.UUID) (fileNames []string, err error)
 }
