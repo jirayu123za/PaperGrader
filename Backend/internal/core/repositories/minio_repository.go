cb387699c407a872d0e42b4ec4b0bac0697d6258
jirayu <jirayu004za@gmail.com>
2024-10-15 01:56:01 +0700

    add repo FindFileFromMinIO

--- Backend/internal/core/repositories/minio_repository.go
+++ Backend/internal/core/repositories/minio_repository.go
@@ -4,4 +4,5 @@ import "mime/multipart"
 
 type MinIORepository interface {
 	AddFileToMinIO(file multipart.File, CourseID, AssignmentID, fileName string) error
+	FindFileFromMinIO(CourseID, AssignmentID, fileName string) (string, error)
 }
