501560085e22eaf4bf6eebc4c04193786189abc4
jirayu <jirayu004za@gmail.com>
2024-10-19 18:12:06 +0700

    add alert in component to desc howdiff to add

--- Frontend/components/AddStudent/AddMember.tsx
+++ Frontend/components/AddStudent/AddMember.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Modal, Button, Divider } from '@mantine/core';
+import { Modal, Button, Divider, Alert } from '@mantine/core';
 import { FaUser, FaUsers } from "react-icons/fa";
 import SingleUser from './SingleUser';
 import CsvFile from './CsvFile';
+import { IconInfoCircle } from '@tabler/icons-react';
 
 interface AddMemberModalProps {
   isOpen: boolean;
@@ -12,6 +13,8 @@ interface AddMemberModalProps {
 const AddMemberModal: React.FC<AddMemberModalProps> = ({ isOpen, onClose }) => {
   const [isSingleUserOpen, setIsSingleUserOpen] = useState(false);
   const [isCsvOpen, setIsCsvOpen] = useState(false);
+
+  const icon = <IconInfoCircle />;
 
   const handleSingleUserOpen = () => {
     setIsSingleUserOpen(true);
@@ -26,25 +29,28 @@ const AddMemberModal: React.FC<AddMemberModalProps> = ({ isOpen, onClose }) => {
   return (
     <>
       <Modal opened={isOpen} onClose={onClose} title="Add Students or Staff">
-      <div className="flex justify-around items-center mt-6">
-          <div className="text-center cursor-pointer" onClick={handleSingleUserOpen}>
-            <FaUser size={50} />
-            <p>Single User</p>
+        <Alert variant="light" color="blue" icon={icon}>
+          Add a single user or upload a CSV file to add multiple users at once.
+        </Alert>
+          <div className="flex justify-around items-center mt-6">
+            <div className="text-center cursor-pointer" onClick={handleSingleUserOpen}>
+              <FaUser size={50} />
+              <p>Single User</p>
+            </div>
+
+            <Divider orientation="vertical" />
+
+            <div className="text-center cursor-pointer" onClick={handleCsvOpen}>
+              <FaUsers size={50} />
+              <p>CSV File</p>
+            </div>
           </div>
 
-          <Divider orientation="vertical" />
-
-          <div className="text-center cursor-pointer" onClick={handleCsvOpen}>
-            <FaUsers size={50} />
-            <p>CSV File</p>
+          <div className="flex justify-end mt-6">
+            <Button variant="filled" color="red" onClick={() => { onClose(); }}>
+              Cancel
+            </Button>
           </div>
-        </div>
-
-        <div className="flex justify-end mt-6">
-          <Button variant="filled" color="red" onClick={() => { onClose(); }}>
-            Cancel
-          </Button>
-        </div>
       </Modal>
 
       <SingleUser isOpen={isSingleUserOpen} onClose={() => setIsSingleUserOpen(false)} />
