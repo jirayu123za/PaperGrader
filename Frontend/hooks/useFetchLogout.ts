586871bf4a73ce7169e98e48b652cd9ba3b31589
jirayu <jirayu004za@gmail.com>
2024-10-19 15:08:35 +0700

    add fetch logout

---
+++ Frontend/hooks/useFetchLogout.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import Router from 'next/router';
+
+export const useFetchLogout = () => {
+    return useQuery({
+        queryKey: ['logout'],
+        queryFn: async () => {
+            try {
+                const response = await axios.post('/api/api/user/logout');
+
+                if (response.status !== 200) {
+                    throw new Error('Network response was not ok');
+                }
+                if (response.status === 200) {
+                    Router.replace('/');
+                } else {
+                    throw new Error('Logout failed');
+                }
+            } catch (error) {
+                console.error('Logout error:', error);
+                throw new Error('An error occurred while logging out');
+            }
+        }
+    });
+};
