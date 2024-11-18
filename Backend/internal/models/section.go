63d188d7c4c09243b29f6a9eb634901e250fac02
jirayu <jirayu004za@gmail.com>
2024-11-19 04:25:08 +0700

    add tb

---
+++ Backend/internal/models/section.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Section struct {
+	SectionID          uuid.UUID           `gorm:"primaryKey" json:"section_id"`
+	CourseID           uuid.UUID           `gorm:"not null" json:"course_id"`
+	Enrollments        []Enrollment        `gorm:"foreignKey:SectionID"`
+	AssignmentSections []AssignmentSection `gorm:"foreignKey:SectionID"`
+	SectionName        string              `gorm:"type:varchar(50);not null" json:"section_name"`
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+	DeletedAt          gorm.DeletedAt `gorm:"index"`
+}
+
+func (section *Section) BeforeCreate(tx *gorm.DB) (err error) {
+	if section.SectionID == uuid.Nil {
+		section.SectionID = uuid.New()
+	}
+	return
+}
