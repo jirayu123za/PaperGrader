1862d7ee9e9ed8693ca0ad6aa31ac60307e52acf
jirayu <jirayu004za@gmail.com>
2024-11-19 04:30:07 +0700

    update repo AddAssignmentWithFiles, FindAssignmentsByCourseID, FindActiveAssignmentsByCourseID, add new repo FindRosterSectionByCourseID

--- Backend/internal/core/repositories/instructor_repository.go
+++ Backend/internal/core/repositories/instructor_repository.go
@@ -8,19 +8,16 @@ import (
 
 // Secondary ports
 type InstructorRepository interface {
-	// Add assignment to course with out Files(Json)
 	AddAssignment(CourseID uuid.UUID, assignment *models.Assignment) error
-	// News add assignment to course with Files(FromData)
-	AddAssignmentWithFiles(CourseID uuid.UUID, assignment *models.Assignment, files []models.AssignmentFile, uploads []models.Upload) error
+	AddAssignmentWithFiles(CourseID uuid.UUID, assignment *models.Assignment, files []models.AssignmentFile, uploads []models.Upload, assignmentSections []models.AssignmentSection) error
 	FindAssignmentNameTemplate(CourseID uuid.UUID, AssignmentID uuid.UUID) (fileName string, err error)
-	//! Find assignment file form student submission
 	FindFileFormSubmission(CourseID uuid.UUID, AssignmentID uuid.UUID) (fileNames []string, err error)
 
 	AddAssignmentFile(file *models.AssignmentFile) error
 
-	// Find instructors and students by course id
 	FindRosterByCourseID(CourseID uuid.UUID) ([]map[string]interface{}, error)
-	// Add student or instructor to course
+	FindRosterSectionByCourseID(CourseID uuid.UUID) ([]map[string]interface{}, error)
+
 	FindUserByEmail(email string) (map[string]interface{}, error)
 	FindInstructorExists(userID uuid.UUID, CourseID uuid.UUID) (bool, error)
 	FindStudentExists(userID, courseID uuid.UUID) (bool, error)
@@ -28,7 +25,7 @@ type InstructorRepository interface {
 	AddStudentToCourse(userID uuid.UUID, courseID uuid.UUID) error
 
 	FindCoursesByUserID(UserID uuid.UUID) ([]map[string]interface{}, error)
-	FindAssignmentsByCourseID(CourseID uuid.UUID) ([]*models.Assignment, error)
-	FindActiveAssignmentsByCourseID(CourseID uuid.UUID) ([]*models.Assignment, error)
+	FindAssignmentsByCourseID(CourseID uuid.UUID) ([]map[string]interface{}, error)
+	FindActiveAssignmentsByCourseID(CourseID uuid.UUID) ([]map[string]interface{}, error)
 	FindInstructorsNameByCourseID(CourseID uuid.UUID) ([]*models.User, error)
 }
