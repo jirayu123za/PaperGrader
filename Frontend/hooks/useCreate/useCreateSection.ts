ee675431e30cccbb7cbfe53cf144a19a9299f70c
jirayu <jirayu004za@gmail.com>
2024-11-20 18:38:18 +0700

    impl hook create section on course

--- Frontend/hooks/useCreate/useCreateSection.ts
+++ Frontend/hooks/useCreate/useCreateSection.ts
@@ -1,12 +1,28 @@
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 
-interface CreateSectionData {
-  name: string;
-}
+const CreateSections = async ({ section_name, course_id }: { section_name: string[], course_id: string }) => {
+  const sectionArray = section_name.map(name => ({ section_name: name }));
 
-export const useCreateSection = () => {
-  return useMutation((data: CreateSectionData) =>
-    axios.post('/api/sections', data) // กำหนด endpoint ของ API ที่จะส่งข้อมูล section
-  );
+  const { data: createSectionsResponse } = await axios.post('/api/api/sections',
+    sectionArray, {
+    params: { course_id },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return createSectionsResponse;
+};
+
+export const useCreateSections = () => {
+  return useMutation({
+    mutationFn: ({ section_name, course_id }: { section_name: string[], course_id: string }) => CreateSections({ section_name, course_id }),
+    onSuccess: (data) => {
+      console.log('Section created successfully:', data);
+    },
+    onError: (error: any) => {
+      console.error('Error creating section:', error);
+    },
+  });
 };
