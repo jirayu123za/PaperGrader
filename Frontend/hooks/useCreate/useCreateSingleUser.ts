923c6b046e3d49ae8ae526cb84ab6cdd321f52b9
jirayu <jirayu004za@gmail.com>
2024-11-26 06:02:25 +0700

    del comment

--- Frontend/hooks/useCreate/useCreateSingleUser.ts
+++ Frontend/hooks/useCreate/useCreateSingleUser.ts
@@ -1,15 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
-// interface UserData {
-//   first_name: string;
-//   last_name: string;
-//   email: string;
-//   student_id?: string;
-//   user_group_name: string;
-//   // notifyUser: boolean;
-// }
-
 const createSingleUser = async (from: FormData) => {
   const course_id = from.get('course_id');
   const { data: response } = await axios.post('/api/api/instructor/roster',
