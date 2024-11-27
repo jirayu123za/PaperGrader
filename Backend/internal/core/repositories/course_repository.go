1a304a85a3ed070bf8fc58be58f7c0951fe262bb
jirayu <jirayu004za@gmail.com>
2024-11-27 17:12:33 +0700

    update repo func AddCourse

--- Backend/internal/core/repositories/course_repository.go
+++ Backend/internal/core/repositories/course_repository.go
@@ -9,24 +9,9 @@ import (
 // Secondary ports
 type CourseRepository interface {
 	// CRUD operations for Courses
-	AddCourse(Course *models.Course) error
+	AddCourse(Course *models.Course, personalData *models.PersonalData, enrollment *models.EnrollmentList) error
 	FindCourseByID(CourseID uuid.UUID) (*models.Course, error)
 	FindCourses() ([]*models.Course, error)
 	ModifyCourse(Course *models.Course) error
 	RemoveCourse(Course *models.Course) error
-
-	// CRUD operations for Courses using jwt
-	/*
-		FindCourseByUserID(UserID uuid.UUID) ([]*models.Course, error)
-		FindNameByUserID(UserID uuid.UUID) (string, error)
-		FindUserGroupByUserID(UserID uuid.UUID) (string, error)
-		FindAssignmentByUserID(UserID uuid.UUID) ([]*models.Assignment, error)
-	*/
-
-	// CRD operations for Instructor lists
-	AddInstructorList(CourseID uuid.UUID, InstructorList *models.InstructorList) error  // Create a new instructor list
-	FindInstructorsList() ([]*models.InstructorList, error)                             // Find all instructor lists
-	FindInstructorsListByCourseID(CourseID uuid.UUID) ([]*models.InstructorList, error) // Find an instructor list by course ID
-	FindInstructorsListByListID(ListID uuid.UUID) (*models.InstructorList, error)       // Find an instructor list by list ID
-	RemoveInstructorList(InstructorList *models.InstructorList) error                   // Remove an instructor list
 }
