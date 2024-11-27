c9ae2309f2474a5b6fbf6b4232945f16a8c14e6f
jirayu <jirayu004za@gmail.com>
2024-11-27 17:19:43 +0700

    update func gorm AddCourse and del func AddInstructorList

--- Backend/internal/adapters/gorm_course_adpter.go
+++ Backend/internal/adapters/gorm_course_adpter.go
@@ -1,6 +1,7 @@
 package adapters
 
 import (
+	"fmt"
 	"paperGrader/internal/models"
 
 	"github.com/google/uuid"
@@ -18,10 +19,33 @@ func NewGormCourseRepository(db *gorm.DB) *GormCourseRepository {
 	}
 }
 
-func (r *GormCourseRepository) AddCourse(Course *models.Course) error {
-	// Implement the logic to AddCourse to the database using GORM.
-	if course := r.db.Create(Course); course.Error != nil {
+func (r *GormCourseRepository) AddCourse(Course *models.Course, personalData *models.PersonalData, enrollment *models.EnrollmentList) error {
+	tx := r.db.Begin()
+
+	if Course.CourseDescription != nil && *Course.CourseDescription == "" {
+		Course.CourseDescription = nil
+	}
+
+	if course := tx.Create(Course); course.Error != nil {
+		tx.Rollback()
 		return course.Error
+	}
+
+	if err := tx.Create(personalData).Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to create personal data: %v", err)
+	}
+
+	enrollment.PersonalDataID = personalData.PersonalDataID
+	enrollment.CourseID = Course.CourseID
+
+	if err := tx.Create(enrollment).Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to create enrollment list: %v", err)
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return fmt.Errorf("failed to commit transaction: %v", err)
 	}
 	return nil
 }
@@ -67,56 +91,6 @@ func (r *GormCourseRepository) RemoveCourse(Course *models.Course) error {
 	}
 
 	if result := r.db.Delete(&findCourse); result.Error != nil {
-		return result.Error
-	}
-	return nil
-}
-
-// Under line here be GormInstructorRepository of Instructor list
-func (r *GormCourseRepository) AddInstructorList(CourseID uuid.UUID, InstructorList *models.InstructorList) error {
-	// Implement the logic to AddInstructorList to the database using GORM.
-	var findCourse *models.Course
-	if result := r.db.First(&findCourse, "course_id = ?", CourseID); result.Error != nil {
-		return result.Error
-	}
-	InstructorList.CourseID = CourseID
-	if instructorList := r.db.Create(InstructorList); instructorList.Error != nil {
-		return instructorList.Error
-	}
-	return nil
-}
-
-func (r *GormCourseRepository) FindInstructorsList() ([]*models.InstructorList, error) {
-	var instructorLists []*models.InstructorList
-	if result := r.db.Find(&instructorLists); result.Error != nil {
-		return nil, result.Error
-	}
-	return instructorLists, nil
-}
-
-func (r *GormCourseRepository) FindInstructorsListByCourseID(CourseID uuid.UUID) ([]*models.InstructorList, error) {
-	var instructorLists []*models.InstructorList
-	if result := r.db.Find(&instructorLists, "course_id = ?", CourseID); result.Error != nil {
-		return nil, result.Error
-	}
-	return instructorLists, nil
-}
-
-func (r *GormCourseRepository) FindInstructorsListByListID(ListID uuid.UUID) (*models.InstructorList, error) {
-	var instructorList *models.InstructorList
-	if result := r.db.First(&instructorList, "list_id = ?", ListID); result.Error != nil {
-		return nil, result.Error
-	}
-	return instructorList, nil
-}
-
-func (r *GormCourseRepository) RemoveInstructorList(InstructorList *models.InstructorList) error {
-	var findInstructorList *models.InstructorList
-	if result := r.db.First(&findInstructorList, "list_id = ?", InstructorList.ListID); result.Error != nil {
-		return result.Error
-	}
-
-	if result := r.db.Delete(&findInstructorList); result.Error != nil {
 		return result.Error
 	}
 	return nil
