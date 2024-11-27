1d0a0ccb6d650c61322312cc7cfd2bac40f2cac6
jirayu <jirayu004za@gmail.com>
2024-11-28 04:07:50 +0700

    add repo AddSections, FindSectionByCourseAndName

--- Backend/internal/core/repositories/section_repository.go
+++ Backend/internal/core/repositories/section_repository.go
@@ -12,4 +12,7 @@ type SectionRepository interface {
 	FindSectionsDetailsByCourseID(CourseID uuid.UUID) ([]*models.Section, error)
 	FindSectionIDsByCourseID(CourseID uuid.UUID) ([]uuid.UUID, error)
 	FindSectionsNameByCourseID(CourseID uuid.UUID) ([]map[string]interface{}, error)
+
+	AddSections(section *models.Section) error
+	FindSectionByCourseAndName(courseID uuid.UUID, sectionName string, section *models.Section) error
 }
