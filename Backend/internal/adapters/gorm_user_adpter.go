173ae449f7bbd54234316a6f125a87fd7c5dcba7
jirayu <jirayu004za@gmail.com>
2024-11-27 17:15:56 +0700

    add func gorm FindPersonByUserID

--- Backend/internal/adapters/gorm_user_adpter.go
+++ Backend/internal/adapters/gorm_user_adpter.go
@@ -3,6 +3,7 @@ package adapters
 import (
 	"paperGrader/internal/models"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -27,6 +28,19 @@ func (r *GormUserRepository) FindUserByGoogleID(googleID string) (*models.User, 
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (r *GormUserRepository) FindPersonByUserID(userID uuid.UUID) ([]map[string]interface{}, error) {
+	var personalData []map[string]interface{}
+
+	if err := r.db.Table("users").
+		Select("users.student_id, users.first_name, users.last_name, users.email, user_groups.group_name AS user_group_name").
+		Joins("LEFT JOIN user_groups ON users.group_id = user_groups.group_id").
+		Where("users.user_id = ? AND users.deleted_at IS NULL", userID).
+		Scan(&personalData).Error; err != nil {
+		return nil, err
+	}
+	return personalData, nil
 }
 
 func (r *GormUserRepository) RemoveJWT(token string) error {
