0f7f8264c71ad5cd40f2df3f7b8d957c681c16fd
jirayu <jirayu004za@gmail.com>
2024-12-04 05:32:22 +0700

    update component for support api

--- Frontend/components/Customize/EditCourseMember.tsx
+++ Frontend/components/Customize/EditCourseMember.tsx
@@ -1,70 +1,99 @@
-import React from 'react';
-import { Modal, Button, TextInput, Select, Skeleton } from '@mantine/core';
+import React, { useEffect } from 'react';
 import SectionSelector from '../Create/Sections/SectionSelector';
-import { useEditCourseMemberStore, useEditCourseMemberFormStore } from '../../store/useEditCourseMemberStore';
+import { Modal, Button, TextInput, Select, Skeleton } from '@mantine/core';
+import { useEditCourseMemberStore } from '../../store/useEditCourseMemberStore';
 import { useSelectSectionStore } from '../../store/useSectionStore';
 import { useFetchEditCourseMember } from '../../hooks/Roster/useFetchEditCourseMember';
+import { useModalEditRosterMemberStore } from '../../store/modal/useRosterModalStore';
+import { useRouter } from 'next/router';
+import { useForm } from '@mantine/form';
 
-interface EditCourseMemberProps {
-  isOpen: boolean;
-  onClose: () => void;
-  course_id: string;
-  personal_data_id: string;
-}
-
-const EditCourseMember: React.FC<EditCourseMemberProps> = ({
-  isOpen,
-  onClose,
-  course_id,
-  personal_data_id,
-}) => {
+const EditCourseMember: React.FC = () => {
+  const router = useRouter();
+  const { course_id } = router.query;
   const { editMember } = useEditCourseMemberStore();
-  const { fullName, studentCode, role, setFullName, setStudentCode, setRole, resetForm } =
-    useEditCourseMemberFormStore();
-  const { selectedSections, setSelectedSections, resetSelectedSections } = useSelectSectionStore();
+  const { selectedSections, setSelectedSections, resetSelectedSections } = useSelectSectionStore();  
+  const { personal_data_id, opened, closeModal } = useModalEditRosterMemberStore();
+  const { isLoading, isSuccess } = useFetchEditCourseMember(course_id as string, personal_data_id as string);
+  // const { mutate: updateCourseMember, status } = useUpdateCourseMember();
 
-  const { isLoading, isSuccess } = useFetchEditCourseMember(course_id, personal_data_id);
-
-  if (isOpen && isSuccess && editMember) {
-    if (fullName === '' && studentCode === '' && role === '') {
-      // อัปเดตค่าฟอร์มเมื่อข้อมูลพร้อมและฟิลด์ยังไม่ได้ตั้งค่า
-      setFullName(editMember.full_name || '');
-      setStudentCode(editMember.student_code || '');
-      setRole(editMember.role_type || '');
-
-      const sections = editMember.section_name
-        ? editMember.section_name.includes(',')
-          ? editMember.section_name.split(',')
-          : [editMember.section_name]
-        : ['All Sections'];
-      setSelectedSections(sections);
-    }
-  }
-
-  const handleSave = () => {
-    const updatedMember = {
-      personal_data_id,
-      full_name: fullName,
-      student_code: studentCode,
-      role_type: role,
-      sections: selectedSections,
-    };
-
-    console.log('Saving member data:', updatedMember);
-    resetForm();
-    resetSelectedSections();
-    onClose();
+  const capitalizeFirstLetter = (value: string) => {
+    return value
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
   };
 
-  const isSectionDisabled = role === 'INSTRUCTOR' || role === 'TA';
+  console.log('Modal state:', { personal_data_id, opened });
+
+  const form = useForm({
+    initialValues: {
+      fullName: '',
+      studentCode: '',
+      role: '',
+      sections: [] as string[],
+    },
+
+    validate: {
+      fullName: (value) => (value ? null : 'Full name is required'),
+      studentCode: (value) => (value ? null : 'Student code is required'),
+      role: (value) => (value ? null : 'Role is required'),
+    },
+  });
+
+  useEffect(() => {
+    if (opened && isSuccess && editMember && form.values.fullName === '') {
+      form.setValues({
+        fullName: editMember.full_name || '',
+        studentCode: editMember.student_code || '',
+        role: editMember.role_type || '',
+        sections: [],
+      });
+  
+      if (editMember.section_name) {
+        const sections = editMember.section_name.includes(',')
+          ? editMember.section_name.split(',')
+          : [editMember.section_name];
+        setSelectedSections(sections);
+      }
+    }
+  }, [opened, isSuccess, editMember]); 
+  
+  const handleSubmit = (values: typeof form.values) => {
+    const formData = new FormData();
+    const name = capitalizeFirstLetter(values.fullName);
+    const [first_name, last_name] = name.split(' ');
+    form.setFieldValue('sections', selectedSections); 
+    
+    formData.append('first_name', first_name || '');
+    formData.append('last_name', last_name || '');
+    formData.append('student_code', values.studentCode);
+    formData.append('role_type', values.role);
+    formData.append('sections', selectedSections.join(','));
+
+    // updateCourseMember({ formData, course_id: course_id, personal_data_id: personal_data_id }, {
+    //   onSuccess: () => {
+    //     resetSelectedSections();
+    //     form.reset();
+    //     closeModal();
+    //   },
+    //   onError: () => {
+    //     console.log('Error updating member');
+    //   },
+    // });
+    form.reset();
+    console.log('Form data:', formData.getAll('first_name'), formData.getAll('last_name'), formData.getAll('student_code'), formData.getAll('role_type'), formData.getAll('sections'));
+  };
+
+  const isSectionDisabled = editMember?.role_type === 'INSTRUCTOR' || editMember?.role_type  === 'TA';
 
   return (
     <Modal
-      opened={isOpen}
+      opened={opened}
       onClose={() => {
-        resetForm();
+        form.reset();
         resetSelectedSections();
-        onClose();
+        closeModal();
       }}
       title="Edit Course Member"
     >
@@ -73,8 +102,7 @@ const EditCourseMember: React.FC<EditCourseMemberProps> = ({
         <Skeleton visible={isLoading}>
           <TextInput
             label="Full Name"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
+            {...form.getInputProps('fullName')}
             required
           />
         </Skeleton>
@@ -84,41 +112,38 @@ const EditCourseMember: React.FC<EditCourseMemberProps> = ({
         <Skeleton visible={isLoading}>
           <TextInput
             label="Student ID"
-            value={studentCode}
-            onChange={(e) => setStudentCode(e.target.value)}
+            {...form.getInputProps('studentCode')}
           />
         </Skeleton>
         <Skeleton visible={isLoading}>
           <Select
             label="Role"
             data={['INSTRUCTOR', 'STUDENT', 'TA']}
-            value={role}
+            {...form.getInputProps('role')}
             onChange={(value) => {
-              setRole(value || '');
+              form.setFieldValue('role', value || '');
               if (value === 'INSTRUCTOR' || value === 'TA') resetSelectedSections();
             }}
             required
           />
         </Skeleton>
         {!isSectionDisabled && (
-          <Skeleton visible={isLoading}>
-            <SectionSelector defaultEnabled={true} />
-          </Skeleton>
+          <SectionSelector defaultEnabled={true} />
         )}
 
         <div className="flex justify-end mt-6">
           <Button
             variant="default"
             onClick={() => {
-              resetForm();
+              form.reset();
               resetSelectedSections();
-              onClose();
+              closeModal();
             }}
-            disabled={isLoading}
+            disabled={status == "pending"}
           >
             Cancel
           </Button>
-          <Button variant="filled" color="teal" className="ml-2" onClick={handleSave} disabled={isLoading}>
+          <Button variant="filled" color="teal" className="ml-2" onClick={() => form.onSubmit(handleSubmit)()} disabled={status == "pending"}>
             Save
           </Button>
         </div>
