bf2d11e5fb875a11edfbe294a4c5fe9b231a0ace
jirayu <jirayu004za@gmail.com>
2024-12-04 05:29:59 +0700

    update del un use code

--- Frontend/store/useEditCourseMemberStore.ts
+++ Frontend/store/useEditCourseMemberStore.ts
@@ -20,24 +20,3 @@ export const useEditCourseMemberStore = create<EditCourseMemberStore>((set) => (
   setEditMember: (member) => set({ editMember: member }),
   resetEditMember: () => set({ editMember: null }),
 }));
-
-
-interface EditCourseMemberFormState {
-  fullName: string;
-  studentCode: string;
-  role: string;
-  setFullName: (value: string) => void;
-  setStudentCode: (value: string) => void;
-  setRole: (value: string) => void;
-  resetForm: () => void;
-}
-
-export const useEditCourseMemberFormStore = create<EditCourseMemberFormState>((set) => ({
-  fullName: '',
-  studentCode: '',
-  role: '',
-  setFullName: (value) => set({ fullName: value }),
-  setStudentCode: (value) => set({ studentCode: value }),
-  setRole: (value) => set({ role: value }),
-  resetForm: () => set({ fullName: '', studentCode: '', role: '' }),
-}));
