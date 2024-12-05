43ed9d381ba74bddad507e4e6e8d67d407208ff7
jirayu <jirayu004za@gmail.com>
2024-12-05 23:55:04 +0700

    impl http handler FindAssignmentByCourseIDAndAssignmentID

--- Backend/internal/adapters/http_instructor_adpter.go
+++ Backend/internal/adapters/http_instructor_adpter.go
@@ -783,6 +783,39 @@ func (h *HttpInstructorHandler) GetActiveAssignmentsByCourseID(c *fiber.Ctx) err
 	})
 }
 
+func (h *HttpInstructorHandler) GetAssignmentByCourseIDAndAssignmentID(c *fiber.Ctx) error {
+	courseIDParam := c.Query("course_id")
+	courseID, err := uuid.Parse(courseIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid course_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignment, err := h.services.GetAssignmentByCourseIDAndAssignmentID(courseID, assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get assignment",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":            "Assignment is retrieved",
+		"assignment_setting": assignment,
+	})
+}
+
 func (h *HttpInstructorHandler) GetInstructorsNameByCourseID(c *fiber.Ctx) error {
 	courseIDParam := c.Query("course_id")
 	courseID, err := uuid.Parse(courseIDParam)
