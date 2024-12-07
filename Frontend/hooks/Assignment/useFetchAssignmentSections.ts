ae718acf1c2a57b9f26eae45e57aead41d55a672
jirayu <jirayu004za@gmail.com>
2024-12-08 00:56:06 +0700

    impl hook useFetchAssignmentSections

---
+++ Frontend/hooks/Assignment/useFetchAssignmentSections.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { useAssignmentSectionsStore } from '../../store/useSectionStore';
+
+interface SectionsList {
+    section_id: string;
+    section_name: string;
+}
+
+export const useFetchAssignmentSections = (assignment_id: string) => {
+    const setAssignmentSections = useAssignmentSectionsStore((state) => state.setAssignmentSections);
+
+    return useQuery<SectionsList[]>({
+        queryKey: ['sections', assignment_id],
+        queryFn: async () => {
+            const response = await axios.get('/api/api/sections/name', {
+                params: { assignment_id: assignment_id },
+            });
+
+            setAssignmentSections(response.data.sections);
+            return response.data.sections || [];
+        },
+        enabled: !!assignment_id,
+    });
+};
