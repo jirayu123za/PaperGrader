b6fadcde04896c252b837e84add542b519f711ae
jirayu <jirayu004za@gmail.com>
2024-12-09 23:40:02 +0700

    del props course_id

--- Frontend/pages/courses/[course_id]/index.tsx
+++ Frontend/pages/courses/[course_id]/index.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 import LeftMain from '../../../components/LeftINS/LeftMain';
 import INTDashBoard from '../../../components/INS/INDDashBoard/INSDashBoard';
 
 const Dashboard = () => {
-  const router = useRouter();
-  const { course_id } = router.query;
-
-  // ตรวจสอบและแปลง course_id ให้เป็น string หรือกำหนดค่าเริ่มต้นเป็น ''
-  const courseId = Array.isArray(course_id) ? course_id[0] : course_id || '';
-
   return (
     <div className="flex min-h-screen bg-gray-50">
       <LeftMain/>
       <div className="flex-grow p-6">
-        <INTDashBoard courseId={courseId} />
+        <INTDashBoard/>
       </div>
     </div>
   );
