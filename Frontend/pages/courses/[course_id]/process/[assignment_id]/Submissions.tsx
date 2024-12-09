3547cd266e8bddc95cce50bfae9c66850072bed5
jirayu <jirayu004za@gmail.com>
2024-12-10 00:24:12 +0700

    update component Submissions

--- Frontend/pages/courses/[course_id]/process/[assignment_id]/Submissions.tsx
+++ Frontend/pages/courses/[course_id]/process/[assignment_id]/Submissions.tsx
@@ -1,39 +1,33 @@
 import { useRouter } from 'next/router';
-import LeftProcess from '../../../../../components/LeftINS/LeftProcess';
 import { useEffect, useState } from 'react';
 import { useAssignmentStore } from '../../../../../store/useAssignmentStore';
-import INSSubmissions from '../../../../../components/INS/INSSubmissions'; // Import INSSubmissions
+import LeftProcess from '../../../../../components/LeftINS/LeftProcess';
+import INSSubmissions from '../../../../../components/INS/INSSubmissions';
 
 export default function Submissions() {
   const router = useRouter();
-  const { assignment_id, course_id } = router.query; // ดึง assignment_id และ course_id มาจาก URL
-  const { assignments } = useAssignmentStore(); // ดึง assignments จาก Zustand Store
+  const { assignment_id, course_id } = router.query;
+  const { assignments } = useAssignmentStore();
   const [loading, setLoading] = useState<boolean>(true);
 
-  // หา assignment_name จาก assignments ใน Zustand Store
   const selectedAssignment = assignments.find((assignment) => assignment.assignment_id === assignment_id);
   const assignmentName = selectedAssignment ? selectedAssignment.assignment_name : 'No Assignment';
 
   useEffect(() => {
-    setLoading(false); // ปิด loading หลังจาก mount component เรียบร้อย
+    setLoading(false);
   }, []);
 
   return (
     <div className="flex min-h-screen">
-      {/* แถบเมนูทางซ้าย */}
-      <LeftProcess assignment_name={assignmentName} course_id={course_id as string} process_id={assignment_id as string} />
+      <LeftProcess assignment_name={assignmentName} process_id={assignment_id as string} />
 
-      {/* ส่วนของการแสดงไฟล์ submissions */}
       <div className="flex-grow p-4">
         {loading ? (
           <div>Loading...</div>
         ) : (
           <>
-            {/* แสดงรายชื่อไฟล์ที่ถูกส่งมาจาก INSSubmissions */}
             <INSSubmissions
-              courseId={course_id as string}
-              assignmentId={assignment_id as string}
-              onViewPDF={() => {}} // ไม่ต้องตั้งค่า URL ของ PDF
+              onViewPDF={() => {}}
             />
           </>
         )}
