8fbcb02384ebe1ae3901158c8aaffbf108b45ef1
jirayu <jirayu004za@gmail.com>
2024-12-10 04:47:35 +0700

    update useFetchLogout use useMutation

--- Frontend/hooks/useFetchLogout.ts
+++ Frontend/hooks/useFetchLogout.ts
@@ -1,28 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import Router from 'next/router';
 
 export const useFetchLogout = () => {
-  return useQuery({
-    queryKey: ['logout'],
-    queryFn: async () => {
+  return useMutation({
+    mutationFn: async () => {
       try {
         const response = await axios.post('/api/api/user/logout');
-
         if (response.status !== 200) {
           throw new Error('Network response was not ok');
         }
-        if (response.status === 200) {
-          Router.replace('/');
-          return response.data || { message: 'Logout successful' };
-        } else {
-          throw new Error('Logout failed');
-        }
+        return response.data || { message: 'Logout successful' };
       } catch (error) {
         console.error('Logout error:', error);
         throw new Error('An error occurred while logging out');
       }
     },
-    enabled: false,
+    onSuccess: () => {
+      Router.replace('/');
+    },
+    onError: (error) => {
+      console.error('Logout failed:', error);
+      alert('Failed to log out. Please try again.');
+    },
   });
 };
