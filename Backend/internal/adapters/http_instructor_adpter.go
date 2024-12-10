18bfc9eac79346e8cf81765fab10be1dd2afe00a
jirayu <jirayu004za@gmail.com>
2024-12-11 06:02:00 +0700

    impl http handler func GetAssignmentDetails

--- Backend/internal/adapters/http_instructor_adpter.go
+++ Backend/internal/adapters/http_instructor_adpter.go
@@ -302,6 +302,39 @@ func (h *HttpInstructorHandler) GetFileFormSubmission(c *fiber.Ctx) error {
 	})
 }
 
+func (h *HttpInstructorHandler) GetAssignmentDetails(c *fiber.Ctx) error {
+	courseIDParam := c.Query("course_id")
+	courseID, err := uuid.Parse(courseIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid course_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignment_details, err := h.services.GetAssignmentDetails(courseID, assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get assignment details",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":            "Assignment details are retrieved",
+		"assignment_details": assignment_details,
+	})
+}
+
 // handler Get instructors and students by course id
 func (h *HttpInstructorHandler) GetRosterByCourseID(c *fiber.Ctx) error {
 	courseIDParam := c.Query("course_id")
