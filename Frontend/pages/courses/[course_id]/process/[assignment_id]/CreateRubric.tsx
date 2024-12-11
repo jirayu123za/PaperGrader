4bad22593130540e652a30f2d42c8771588f448f
pulom999 <pulom999@gmail.com>
2024-12-12 01:51:31 +0700

    add page&component

--- Frontend/pages/courses/[course_id]/process/[assignment_id]/CreateRubric.tsx
+++ Frontend/pages/courses/[course_id]/process/[assignment_id]/CreateRubric.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import LeftProcess from '../../../../../components/LeftINS/LeftProcess';
 import INSCreateRubric from '../../../../../components/INS/INSProcess/INSCreateRubric';
 
-export default function CreateRubric() {
-    return (
-        <div className="flex min-h-screen bg-gray-50">
-            <LeftProcess />
-            <div className="flex-grow p-6">
-                <INSCreateRubric />
-            </div>
-        </div>
-    );
+export default function CreateRubricPage() {
+  return (
+    <div className="flex min-h-screen">
+      <LeftProcess />
+      <div className="flex-grow p-4">
+        <INSCreateRubric />
+      </div>
+    </div>
+  );
 }
