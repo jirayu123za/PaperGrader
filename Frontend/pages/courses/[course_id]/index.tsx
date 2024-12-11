2d74657e9c28d19564c0ef455df16019ffbb6d24
pulom999 <pulom999@gmail.com>
2024-12-11 23:41:55 +0700

    update import

--- Frontend/pages/courses/[course_id]/index.tsx
+++ Frontend/pages/courses/[course_id]/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import LeftMain from '../../../components/LeftINS/LeftMain';
-import INTDashBoard from '../../../components/INS/INDDashBoard/INSDashBoard';
+import INTDashBoard from '../../../components/INS/INSDashBoard/INSDashBoard';
 
 const Dashboard = () => {
   return (
     <div className="flex min-h-screen bg-gray-50">
-      <LeftMain/>
+      <LeftMain />
       <div className="flex-grow p-6">
-        <INTDashBoard/>
+        <INTDashBoard />
       </div>
     </div>
   );
