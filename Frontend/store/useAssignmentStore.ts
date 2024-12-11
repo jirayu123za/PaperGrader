3bf985cccc3535f492913496d8c00105fd997559
jirayu <jirayu004za@gmail.com>
2024-12-12 02:10:34 +0700

    impl useInsAssignmentStore

--- Frontend/store/useAssignmentStore.ts
+++ Frontend/store/useAssignmentStore.ts
@@ -1,11 +1,8 @@
 import { create } from 'zustand';
 
-// สร้าง interface สำหรับ assignment
 interface Assignment {
   assignment_id: string;
-  //CourseId: string;
   assignment_name: string;
-  //assignment_decription: string;
   assignment_release_date: string;
   assignment_due_date: string;
   assignment_cut_off_date: string;
@@ -14,7 +11,6 @@ interface Assignment {
   submiss_by: string;
 }
 
-// สร้าง Zustand store สำหรับ assignments
 interface AssignmentStore {
   assignments: Assignment[];
   setAssignments: (assignments: Assignment[]) => void;
@@ -23,4 +19,34 @@ interface AssignmentStore {
 export const useAssignmentStore = create<AssignmentStore>((set) => ({
   assignments: [],
   setAssignments: (assignments) => set({ assignments }),
+}));
+
+interface AssignmentSection {
+  assignment_section_id: string;
+  cut_off_date: string | null;
+  due_date: string | null;
+  release_date: string | null;
+  section_id: string;
+  section_name: string;
+}
+
+interface InsAssignment {
+  assignment_due_date: string | null;
+  assignment_id: string;
+  assignment_name: string;
+  assignment_release_date: string | null;
+  assignment_sections: AssignmentSection[];
+  published: boolean;
+  regrades: boolean;
+  submiss_by: string;
+}
+
+interface InsAssignmentStore {
+  insAssignments: InsAssignment[];
+  setInsAssignments: (insAssignments: InsAssignment[]) => void;
+}
+
+export const useInsAssignmentStore = create<InsAssignmentStore>((set) => ({
+  insAssignments: [],
+  setInsAssignments: (insAssignments) => set({ insAssignments }),
 }));
