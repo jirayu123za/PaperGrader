4cc95e5d99d803d376c15d80eb6d0e9ac57d3014
jirayu <jirayu004za@gmail.com>
2024-12-12 15:51:05 +0700

    update reset expandedAssignments

--- Frontend/store/Table/useInsAssignmentTableStore.ts
+++ Frontend/store/Table/useInsAssignmentTableStore.ts
@@ -7,11 +7,16 @@ interface AssignmentExpandState {
 
 export const useAssignmentExpandStore = create<AssignmentExpandState>((set) => ({
     expandedAssignments: {},
-    toggleAssignment: (assignment_id) =>
-        set((state) => ({
-            expandedAssignments: {
-                ...state.expandedAssignments,
-                [assignment_id]: !state.expandedAssignments[assignment_id],
-            },
-        })),
+
+    toggleAssignment: (assignment_id: string) =>
+        set((state) => {
+            const isExpanded = state.expandedAssignments[assignment_id];
+            const newExpandedAssignments = isExpanded
+                ? {}
+                : { [assignment_id]: true };
+
+            return {
+                expandedAssignments: newExpandedAssignments,
+            };
+        }),
 }));
