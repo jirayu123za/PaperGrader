323e1149654576dbe8df42bd4f9a6acaf37c539b
jirayu <jirayu004za@gmail.com>
2024-12-13 05:25:04 +0700

    update store for support send value to form

--- Frontend/store/modal/useAssignmentSettingModal.ts
+++ Frontend/store/modal/useAssignmentSettingModal.ts
@@ -40,3 +40,32 @@ export const useModalAssignmentSettingStore = create<AssignmentSetting>((set) =>
         set({ assignment_id: '', opened: false });
     },
 }));
+
+interface CustomizeTimeState {
+    release_date: string;
+    due_date: string;
+    cut_off_date: string;
+    selectedSections: string[];
+    setCustomizeTime: (values: {
+        release_date: string;
+        due_date: string;
+        cut_off_date: string;
+        selectedSections: string[];
+    }) => void;
+    resetCustomizeTime: () => void;
+}
+
+export const useCustomizeTimeStore = create<CustomizeTimeState>((set) => ({
+    release_date: '',
+    due_date: '',
+    cut_off_date: '',
+    selectedSections: [],
+    setCustomizeTime: (values) => set(values),
+    resetCustomizeTime: () =>
+        set({
+            release_date: '',
+            due_date: '',
+            cut_off_date: '',
+            selectedSections: [],
+        }),
+}));
