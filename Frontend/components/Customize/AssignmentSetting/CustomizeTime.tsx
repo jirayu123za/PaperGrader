ad9ac8b4499bbbb076df2c0005e8ea0dc88a77e3
jirayu <jirayu004za@gmail.com>
2024-12-13 05:27:12 +0700

    update use store and init values

--- Frontend/components/Customize/AssignmentSetting/CustomizeTime.tsx
+++ Frontend/components/Customize/AssignmentSetting/CustomizeTime.tsx
@@ -1,52 +1,49 @@
 import React, { useEffect } from 'react';
-import { Button, Checkbox } from '@mantine/core';
 import { DateTimePicker } from '@mantine/dates';
 import { useForm } from '@mantine/form';
 import { useRouter } from 'next/router';
 import { useFetchAssignmentSetting } from '../../../hooks/AssignmentSetting/useFetchAssignmentSetting'; 
 import { useAssignmentSettingStore } from '../../../store/useAssignmentSettingStore';
-import { useModalAssignmentSettingStore } from '../../../store/modal/useAssignmentSettingModal';
+import { useCustomizeTimeStore, useModalAssignmentSettingStore } from '../../../store/modal/useAssignmentSettingModal';
+import { useSelectSectionStore } from '../../../store/useSectionStore';
 import SectionEditAssignment from '../../Create/Sections/SectionEditAssignment';
 import '@mantine/dates/styles.css';
 
 const CustomizeTime: React.FC = () => {
   const router = useRouter();
   const { course_id } = router.query;
-  const { assignmentSetting } = useAssignmentSettingStore();
   const { assignment_id } = useModalAssignmentSettingStore(state => state);
+  const { assignmentSetting } = useAssignmentSettingStore();
+  const { selectedSections } = useSelectSectionStore();
+  const { setCustomizeTime } = useCustomizeTimeStore();
   const { isLoading } = useFetchAssignmentSetting(course_id as string, assignment_id as string);
 
   const form = useForm({
     initialValues: {
-      selectedSections: assignmentSetting?.assignmentSections.map((s) => s.section_id) || [],
       release_date: assignmentSetting?.assignmentSections[0]?.releaseDate || '',
       due_date: assignmentSetting?.assignmentSections[0]?.dueDate || '',
       cut_off_date: assignmentSetting?.assignmentSections[0]?.cutOffDate || '',
-      allowLateSubmissions: !!assignmentSetting?.assignment?.lateSubmiss || false,
     },
   });
 
   useEffect(() => {
     if (assignmentSetting) {
-      form.setValues({
-        selectedSections: assignmentSetting.assignmentSections.map((s) => s.section_id),
+      const initialValues = {
         release_date: assignmentSetting.assignmentSections[0]?.releaseDate || '',
         due_date: assignmentSetting.assignmentSections[0]?.dueDate || '',
         cut_off_date: assignmentSetting.assignmentSections[0]?.cutOffDate || '',
-        allowLateSubmissions: !!assignmentSetting.assignment?.lateSubmiss || false,
-      });
+        sections: selectedSections || [],
+      };
+      form.setValues(initialValues);
+      setCustomizeTime({ ...initialValues, selectedSections });
     }
-  }, [assignmentSetting]);
+  }, [assignmentSetting, setCustomizeTime]);
 
-  // const handleSubmit = (values: typeof form.values) => {
-  //   console.log('Updated Customize Time:', {
-  //     sections: values.selectedSections,
-  //     releaseDate: values.release_date,
-  //     dueDate: values.due_date,
-  //     cutOffDate: values.allowLateSubmissions ? values.cut_off_date : '',
-  //   });
-  //   // TODO: call API to update assignment setting
-  // };
+  useEffect(() => {
+    const updatedValues = { ...form.values, selectedSections };
+    setCustomizeTime(updatedValues);
+    form.setFieldValue('sections', selectedSections);
+  }, [form.values, selectedSections, setCustomizeTime]);
 
   return (
     <>
