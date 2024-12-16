7964706e18d12deca5edce3e96b487d9671dd0d4
jirayu <jirayu004za@gmail.com>
2024-12-17 04:31:11 +0700

    update interface StudentAssignment

--- Frontend/store/useSTD_AssignmentStore.ts
+++ Frontend/store/useSTD_AssignmentStore.ts
@@ -1,12 +1,16 @@
 import { create } from 'zustand';
 
 interface StudentAssignment {
+  course_id: string;
   assignment_id: string;
   course_code: string;
   course_name?: string;
   assignment_name: string;
+  assignment_description: string;
+  cut_off_date: string;
   due_date: string;
   release_Date: string;
+  section_name: string;
 }
 
 interface AssignmentStore {
