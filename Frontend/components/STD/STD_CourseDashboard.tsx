c8ab0c48cdafede249fb81c5c936ae637753f76a
jirayu <jirayu004za@gmail.com>
2024-12-17 17:42:27 +0700

    change key table, using dayjs

--- Frontend/components/STD/STD_CourseDashboard.tsx
+++ Frontend/components/STD/STD_CourseDashboard.tsx
@@ -3,14 +3,16 @@ import { useFetchAssignments } from '../../hooks/useFetchAssignments';
 import { useAssignmentStore } from '../../store/useAssignmentStore';
 import { useStdCourseDashboardStore } from '../../store/useCourseStore';
 import { useRouter } from 'next/router';
-import { Badge, Divider, Table } from '@mantine/core';
+import { Badge, Divider, Table, Title } from '@mantine/core';
 import { useFetchStdCourse } from '../../hooks/useFetchCourse';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 
 const STD_CourseDashboard: React.FC = () => {
   const router = useRouter();
+  dayjs.extend(utc);
   const { course_id } = router.query;
-  const { data: assignments, isLoading, error } = useFetchAssignments(course_id as string);
+  const { isLoading, error } = useFetchAssignments(course_id as string);
   const { assignments: assignmentList } = useAssignmentStore();
   const { isLoading: isCourseLoading, error: errorCourse } = useFetchStdCourse(course_id as string);
   const { course: courseData } = useStdCourseDashboardStore();
@@ -21,9 +23,9 @@ const STD_CourseDashboard: React.FC = () => {
   return (
     <div className="course-dashboard">
       <div className="header mb-6">
-        <h1 className="text-3xl font-bold">
+        <Title order={2}>
           {courseData?.course_name} | {courseData?.semester} / {courseData?.academic_year}
-        </h1>
+        </Title>
         <p className="text-gray-500">Course Code: {courseData?.course_code}</p>
         <Divider my="md" />
       </div>
@@ -33,13 +35,14 @@ const STD_CourseDashboard: React.FC = () => {
           This course has no assignments assigned yet.
         </div>
       ) : (
-        <Table className="min-w-full bg-white border-collapse">
+        <Table highlightOnHover verticalSpacing="md">
           <Table.Thead>
             <Table.Tr className="border-b">
-              <Table.Th className="py-2 px-4 text-left">Name</Table.Th>
-              <Table.Th className="py-2 px-4 text-left">Status</Table.Th>
-              <Table.Th className="py-2 px-4 text-left">Released</Table.Th>
-              <Table.Th className="py-2 px-4 text-left">Due</Table.Th>
+              <Table.Th>NAME</Table.Th>
+              <Table.Th style={{ textAlign: 'center' }}>RELEASED</Table.Th>
+              <Table.Th style={{ textAlign: 'center' }}>DUE</Table.Th>
+              <Table.Th style={{ textAlign: 'center' }}>LAST DUE</Table.Th>
+              <Table.Th style={{ textAlign: 'center' }}>STATUS</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
@@ -55,20 +58,22 @@ const STD_CourseDashboard: React.FC = () => {
                     >
                       {assignment.assignment_name}
                     </Table.Td>
-                    <Table.Td className="py-2 px-4">
+                    <Table.Td style={{ textAlign: 'center' }}>
+                      {assignment.release_date 
+                        ? dayjs(assignment.release_date).utc().format('MMM D, YYYY h:mm A'): 'N/A'}
+                    </Table.Td>
+                    <Table.Td style={{ textAlign: 'center' }}>
+                      {assignment.due_date
+                        ? dayjs(assignment.due_date).utc().format('MMM D, YYYY h:mm A'): 'N/A'}
+                    </Table.Td>
+                    <Table.Td style={{ textAlign: 'center' }}>
+                      {assignment.cut_off_date
+                        ? dayjs(assignment.cut_off_date).utc().format('MMM D, YYYY h:mm A'): 'N/A'}
+                    </Table.Td>
+                    <Table.Td style={{ textAlign: 'center' }}>
                       <Badge color={status === 'Submitted' ? 'green' : 'blue'} variant="filled">
                         {status === 'Submitted' ? 'Submitted' : 'No Submission'}
                       </Badge>
-                    </Table.Td>
-                    <Table.Td className="py-2 px-4">{assignment.assignment_release_date}</Table.Td>
-                    <Table.Td className="py-2 px-4">
-                      <div>
-                        {assignment.assignment_due_date}
-                        <br />
-                        <span className="text-gray-500">
-                          Late Due Date: {assignment.assignment_due_date}
-                        </span>
-                      </div>
                     </Table.Td>
                   </Table.Tr>
                 </React.Fragment>
