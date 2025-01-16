f95581a5599113380550b40212f0dbc9430bb38a
jirayu <jirayu004za@gmail.com>
2025-01-17 00:33:09 +0700

    change spell word to INSManageScansPage

--- Frontend/pages/courses/[course_id]/process/[assignment_id]/ManageScans.tsx
+++ Frontend/pages/courses/[course_id]/process/[assignment_id]/ManageScans.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LeftProcess from '../../../../../components/LeftINS/LeftProcess';
 import INSManageScans from '../../../../../components/INS/INSProcess/INSManageScans';
 
-export default function INSManageScanspage() {
+export default function INSManageScansPage() {
   return (
     <div className="flex min-h-screen">
       <LeftProcess />
