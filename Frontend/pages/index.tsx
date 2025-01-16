ebb83c6bbec757ad66714c575b0d044cfee8473d
jirayu <jirayu004za@gmail.com>
2025-01-16 15:05:23 +0700

    update landing page

--- Frontend/pages/index.tsx
+++ Frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { AppShell, Button, Container, Flex, Grid, Group, Text, Title } from "@mantine/core";
+import { AppShell, Button, Container, Flex, Grid, Group, Image, Text, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import SignUp from "../components/Login/SignUp";
 import SignIn from "../components/Login/SignIn";
@@ -29,36 +29,41 @@ export default function LandingPage() {
         footer: { backgroundColor: "#141414" },
       }}
     >
-      <AppShell.Header className="flex justify-between items-center px-5">
-        <Title className="text-black">LOGO</Title>
-        <Group gap="xl">
-          <Text component="a" href="#" className="text-gray-700">
-            Get start
-          </Text>
-          <Text component="a" href="#" className="text-gray-700">
-            Get demo
-          </Text>
-          <Button className='shadow-md' onClick={openSignUp}>Sign Up</Button>
-        </Group>
+      <AppShell.Header>
+        <Flex justify="space-between" align="center" px="lg" h={80}>
+          <Image src="/Image/logo-ppgd.png" alt="logo" w="auto" h={80} p={8} />
+          <Group gap="xl" p={8}>
+            <Text component="a" href="#" className="text-gray-700">
+              Get start
+            </Text>
+            <Text component="a" href="#" className="text-gray-700">
+              Get demo
+            </Text>
+            <Button className="shadow-md" onClick={openSignUp}>
+              Sign Up
+            </Button>
+          </Group>
+        </Flex>
       </AppShell.Header>
 
-      {/* Main Content Section */}
       <AppShell.Main className="flex-col flex justify-center items-center">
       <Container>
         <Flex direction="column" align="center" justify="center" style={{ textAlign: "center" }}>
-          <Text size="64px" w={1400} fw={500} c="#424242">
+          <Text size="64px" w={1400} fw={500} c="#424242" style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.1)" }}>
             Transform the way you grade easier than ever
           </Text>
           <div className="flex justify-center items-center mt-6">
-            <Text size="64px" w={150} fw={500} c="#424242">
+            <Text size="64px" w={150} fw={500} h={70} c="#424242" style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.1)" }}>
               with
             </Text>
             <Text
               variant="gradient"
-              gradient={{ from: "#B7410E", to: "#F0A369", deg: 45 }}
+              gradient={{ from: "#B7410E", to: "#F0A369", deg: 45 } }
               size="64px"
               fw={500}
               w="auto"
+              h={70}
+              style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.1)" }}
             >
               PaperGrader
             </Text>
