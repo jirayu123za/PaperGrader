f4cf7463a25c636c4b3f5a17a0cef95598505922
jirayu <jirayu004za@gmail.com>
2025-01-21 11:29:07 +0700

    add relation fk AssignmentID on tb Rubrics

--- Backend/internal/models/assignment.go
+++ Backend/internal/models/assignment.go
@@ -22,6 +22,7 @@ type Assignment struct {
 	Submissions           []Submission        `gorm:"foreignKey:AssignmentID"`
 	AssignmentSections    []AssignmentSection `gorm:"foreignKey:AssignmentID"`
 	BoundingBoxes         []BoundingBox       `gorm:"foreignKey:AssignmentID"`
+	Rubrics               []Rubric            `gorm:"foreignKey:AssignmentID"`
 	CreatedAt             time.Time
 	UpdatedAt             time.Time
 	DeletedAt             gorm.DeletedAt `gorm:"index"`
