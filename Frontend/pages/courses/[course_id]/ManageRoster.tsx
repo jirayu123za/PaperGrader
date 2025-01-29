9ce6be4e429a500734971e1190cdd081e89b1d2c
jirayu <jirayu004za@gmail.com>
2025-01-30 02:18:38 +0700

    change button to tabs

--- Frontend/pages/courses/[course_id]/ManageRoster.tsx
+++ Frontend/pages/courses/[course_id]/ManageRoster.tsx
@@ -1,47 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LeftMain from '../../../components/LeftINS/LeftMain';
 import CourseRoster from '../../../components/ManageRoster/CourseRoster';
-import ManageSection from '../../../components/ManageRoster/[course_id]/ManageSection';
-import CreateSection from '../../../components/Create/CreateSection';
-import { useRouter } from 'next/router';
-import { useCourseStore } from '../../../store/useCourseStore';
-import { Button } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import ManageSection from '../../../components/ManageRoster/ManageSection';
+import { Tabs } from '@mantine/core';
 
 const ManageRoster: React.FC = () => {
-  const router = useRouter();
-  const { courseId } = router.query;
-  const selectedCourseId = useCourseStore((state) => state.selectedCourseId);
-  const actualCourseId = Array.isArray(courseId) ? courseId[0] : courseId || selectedCourseId;
-  const [isRosterView, { open: showRoster, close: showSection }] = useDisclosure(true);
-
-  if (!actualCourseId) {
-    return <div>Please select a course first!</div>;
-  }
-
   return (
     <div className="flex min-h-screen bg-gray-50">
       <LeftMain/>
       <div className="flex-grow p-6">
-
-        {/* ปุ่มเพื่อสลับระหว่าง CourseRoster และ ManageSection ชิดขวา */}
-        <div className="flex justify-end space-x-4 mb-4">
-          <Button
-            variant={isRosterView ? 'filled' : 'outline'}
-            onClick={showRoster}
-          >
-            Manage Roster
-          </Button>
-          <Button
-            variant={!isRosterView ? 'filled' : 'outline'}
-            onClick={showSection}
-          >
-            Manage Section
-          </Button>
-        </div>
-
-        {isRosterView ? <CourseRoster /> : <ManageSection />}
-        {!isRosterView && ( <CreateSection />)}
+        <Tabs defaultValue="roster">
+          <Tabs.List>
+            <Tabs.Tab value="roster">Manage roster</Tabs.Tab>
+            <Tabs.Tab value="section">Manage section</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel value="roster">
+            <CourseRoster />
+          </Tabs.Panel>
+          <Tabs.Panel value="section">
+            <ManageSection />
+          </Tabs.Panel>
+        </Tabs>
       </div>
     </div>
   );
