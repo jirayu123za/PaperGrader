283363a9883f31e58ddbffef4b437d327f32d23d
jirayu <jirayu004za@gmail.com>
2025-02-21 18:23:29 +0700

    add repo FindTemplatePageCountFromMinIO

--- Backend/internal/core/repositories/minio_repository.go
+++ Backend/internal/core/repositories/minio_repository.go
@@ -6,4 +6,6 @@ type MinIORepository interface {
 	AddFileToMinIO(file multipart.File, CourseID, AssignmentID, fileName string) error
 	FindFileFromMinIO(CourseID, AssignmentID, fileName string) (string, error)
 	FindFilesAndNames(CourseID, AssignmentID, fileNames []string) ([]string, []string, error)
+
+	FindTemplatePageCountFromMinIO(CourseID, AssignmentID, fileName string) (int, error)
 }
