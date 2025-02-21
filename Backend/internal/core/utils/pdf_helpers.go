9cb8878a8f67874e2e8701d3617286d132b9e1e8
jirayu <jirayu004za@gmail.com>
2025-02-22 03:46:24 +0700

    update ExtractPDFPages use ExtractPagesFile, MergeCreateFile

--- Backend/internal/core/utils/pdf_helpers.go
+++ Backend/internal/core/utils/pdf_helpers.go
@@ -16,8 +16,38 @@ func GetPDFPageCount(filePath string) (int, error) {
 	return ctx.PageCount, nil
 }
 
-func ExtractPDFPages(inputPath, outputPath string, startPage, endPage int) error {
-	outputPath = filepath.Clean(outputPath)
-	os.MkdirAll(outputPath, os.ModePerm)
-	return api.ExtractPagesFile(inputPath, outputPath, []string{fmt.Sprintf("%d-%d", startPage, endPage)}, nil)
+func ExtractPDFPages(inputPath string, startPage, endPage int) (string, error) {
+	tempDir, err := os.MkdirTemp("", "pdf_pages")
+	if err != nil {
+		return "", err
+	}
+
+	err = api.ExtractPagesFile(inputPath, tempDir, []string{fmt.Sprintf("%d-%d", startPage, endPage)}, nil)
+	if err != nil {
+		os.RemoveAll(tempDir)
+		return "", err
+	}
+
+	mergedFilePath := filepath.Join(os.TempDir(), fmt.Sprintf("merged_%d_%d.pdf", startPage, endPage))
+
+	files, err := os.ReadDir(tempDir)
+	if err != nil {
+		os.RemoveAll(tempDir)
+		return "", err
+	}
+
+	var filePaths []string
+	for _, file := range files {
+		filePaths = append(filePaths, filepath.Join(tempDir, file.Name()))
+	}
+
+	err = api.MergeCreateFile(filePaths, mergedFilePath, false, nil)
+	if err != nil {
+		os.RemoveAll(tempDir)
+		return "", err
+	}
+
+	os.RemoveAll(tempDir)
+
+	return mergedFilePath, nil
 }
