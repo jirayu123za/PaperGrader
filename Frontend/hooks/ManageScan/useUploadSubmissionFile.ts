59bc964cababf81bb54b12116bb01f4c4e8f0ac1
jirayu <jirayu004za@gmail.com>
2025-02-23 01:30:27 +0700

    change mock to using API

--- Frontend/hooks/ManageScan/useUploadSubmissionFile.ts
+++ Frontend/hooks/ManageScan/useUploadSubmissionFile.ts
@@ -9,33 +9,32 @@ interface UploadFileParams {
 
 const uploadSubmissionFile = async ({ assignment_id, course_id, files }: UploadFileParams) => {
     console.log('Uploading submission files:', files, 'for assignment:', assignment_id, 'and course:', course_id);
-    // const formData = new FormData();
-    // files.forEach((file) => formData.append('files', file));
+    const formData = new FormData();
+    files.forEach((file) => formData.append('files', file));
 
-    // const { data } = await axios.post(`/api/api/`, formData, {
-    //     headers: {
-    //         'Content-Type': 'multipart/form-data',
-    //     },
-    //     params: {
-    //         assignment_id: assignment_id,
-    //         course_id: course_id,
-    //     },
-    // });
+    const { data } = await axios.post(`/api/api/instructor/submission/file`, formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+        params: {
+            assignment_id: assignment_id,
+            course_id: course_id,
+        },
+    });
 
-    // return data;
-    return { success: true, message: "Mock upload complete!" };
+    return data;
 };
 
 export const useUploadSubmissionFile = () => {
     return useMutation({
         mutationFn: uploadSubmissionFile,
-        // onSuccess: (data) => {
-        //     console.log('Upload successful:', data);
-        //     // alert(`File uploaded successfully! Submission ID: ${data.submission.SubmissionID}`);
-        // },
-        // onError: (error) => {
-        //     console.error('Upload failed:', error);
-        //     // alert('Failed to upload the file. Please try again.');
-        // },
+        onSuccess: (data) => {
+            console.log('Upload successful:', data);
+            alert(`File uploaded successfully!`);
+        },
+        onError: (error) => {
+            console.error('Upload failed:', error);
+            alert('Failed to upload the file. Please try again.');
+        },
     });
 };
