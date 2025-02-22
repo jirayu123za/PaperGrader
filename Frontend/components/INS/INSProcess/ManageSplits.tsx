9e8ef21a0fff3565b7222f42074ff1ea76bab132
jirayu <jirayu004za@gmail.com>
2025-02-23 05:48:57 +0700

    add ScrollArea, Combobox, message

--- Frontend/components/INS/INSProcess/ManageSplits.tsx
+++ Frontend/components/INS/INSProcess/ManageSplits.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Anchor, Badge, Box, Flex, Select, Table, TextInput, Image, Text, Pagination, Autocomplete, ActionIcon, Stack } from '@mantine/core';
+import { Anchor, Badge, Box, Flex, Select, Table, TextInput, Image, Text, Pagination, Autocomplete, ActionIcon, Stack, ScrollArea, useCombobox, Combobox } from '@mantine/core';
 import { usePagination } from '@mantine/hooks';
 import { IconSearch, IconEdit } from '@tabler/icons-react';
 import { RiDeleteBinLine } from "react-icons/ri";
@@ -31,7 +31,7 @@ export const ManageSplits = () => {
     const router = useRouter();
     const { assignment_id, course_id } = router.query;
     const { isLoading, error } = useFetchStudentsList(course_id as string, assignment_id as string);
-    const { studentsList } = useStudentsListStore();
+    const { studentsList, pageSize, setPageSize } = useStudentsListStore();
 
     const [submissions, setSubmissions] = useState(mockSubmissions);
     const [searchQuery, setSearchQuery] = useState('');
@@ -42,7 +42,6 @@ export const ManageSplits = () => {
         (!searchQuery || sub.studentName?.toLowerCase().includes(searchQuery.toLowerCase()))
     );
 
-    const pageSize = 5;
     const totalPages = Math.ceil(filteredSubmissions.length / pageSize);
     const pagination = usePagination({
         total: totalPages,
@@ -55,7 +54,10 @@ export const ManageSplits = () => {
         (pagination.active - 1) * pageSize,
         pagination.active * pageSize
     );
-
+    
+    const message = `Showing ${pageSize * (pagination.active - 1) + 1} – ${Math.min(filteredSubmissions.length, pageSize * pagination.active)} of ${filteredSubmissions.length}`;
+    const combobox = useCombobox();
+    
     const handleStudentNameChange = (id: string, name: string) => {
         setSubmissions((prev) =>
             prev.map((submission) => (submission.id === id ? { ...submission, studentName: name } : submission))
@@ -116,91 +118,125 @@ export const ManageSplits = () => {
             </Flex>
 
             <Box>
-                <Table highlightOnHover w="100%" miw='900px'>
-                    <Table.Thead>
-                        <Table.Tr>
-                            <Table.Th w='30%'>Name & ID Region</Table.Th>
-                            <Table.Th w="20%">Student</Table.Th>
-                            <Table.Th w="15%">Sections Submitted</Table.Th>
-                            <Table.Th w="15%">Submission Time</Table.Th>
-                            <Table.Th w="10%" ta='center'>Graded</Table.Th>
-                            <Table.Th w="10%" ta='center'>Delete Submission</Table.Th>
-                        </Table.Tr>
-                    </Table.Thead>
-                    <Table.Tbody>
-                        {paginatedData.map((submission) => (
-                            <Table.Tr key={submission.id}>
-                                <Table.Td>
-                                    <Image src={submission.imageUrl} alt="Submission" w='600px' h='100px' />
-                                </Table.Td>
-                                <Table.Td>
-                                    {submission.studentName ? (
-                                        <Stack gap={1}>
-                                            <Flex align="center">
-                                                <Text>{submission.studentName}</Text>
-                                                <ActionIcon variant="transparent" ml={8} aria-label="Edit Student Name" className='cursor-pointer'>
-                                                    <IconEdit size={16}/>
-                                                </ActionIcon>
-                                            </Flex>
-                                            <Text size='sm' c="dimmed">{submission.id}</Text>
-                                        </Stack>
-                                    ) : (
-                                        <Autocomplete
-                                            placeholder="Select student or enter name"
-                                            // data={studentsList.map((student) => ({
-                                            //     value: student.personal_data_id,
-                                            //     label: student.full_name,
-                                            //   }))}
-                                            styles={{
-                                                option: {
-                                                  minHeight: '40px',
-                                                },
-                                            }}
-                                            data={autocompleteData}
-                                            limit={10}
-                                            maxDropdownHeight={200}
-                                            comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
-                                        />                                        
-                                    )}
-                                </Table.Td>
-                                <Table.Td>
-                                    {submission.sectionsSubmitted !== '-' ? (
-                                        <Text>{submission.sectionsSubmitted}</Text>
-                                    ) : (
-                                        // <Select
-                                        //     placeholder="Select section"
-                                        //     data={['801', '802', '803']}
-                                        //     onChange={(value) => handleStudentNameChange(submission.id, value || '')}
-                                        // />
-                                        <></>
-                                    )}
-                                </Table.Td>
-                                <Table.Td>
-                                    <Text>{submission.submissionTime}</Text>
-                                </Table.Td>
-                                <Table.Td ta='center'>
-                                    <Badge w={52} color={submission.graded > 0 ? 'orange' : 'red'}>
-                                        {submission.graded}%
-                                    </Badge>
-                                </Table.Td>
-                                <Table.Td ta='center'>
-                                    <ActionIcon variant="transparent" aria-label="Delete Submission">
-                                        <RiDeleteBinLine size={20} />
-                                    </ActionIcon>
-                                </Table.Td>
+                <ScrollArea h="612px">
+                    <Table highlightOnHover w="100%" miw='900px'>
+                        <Table.Thead>
+                            <Table.Tr>
+                                <Table.Th w='30%'>Name & ID Region</Table.Th>
+                                <Table.Th w="20%">Student</Table.Th>
+                                <Table.Th w="15%">Sections Submitted</Table.Th>
+                                <Table.Th w="15%">Submission Time</Table.Th>
+                                <Table.Th w="10%" ta='center'>Graded</Table.Th>
+                                <Table.Th w="10%" ta='center'>Delete Submission</Table.Th>
                             </Table.Tr>
-                        ))}
-                    </Table.Tbody>
-                </Table>
+                        </Table.Thead>
+                        <Table.Tbody>
+                            {paginatedData.map((submission) => (
+                                <Table.Tr key={submission.id}>
+                                    <Table.Td>
+                                        <Image src={submission.imageUrl} alt="Submission" w='600px' h='100px' />
+                                    </Table.Td>
+                                    <Table.Td>
+                                        {submission.studentName ? (
+                                            <Stack gap={1}>
+                                                <Flex align="center">
+                                                    <Text>{submission.studentName}</Text>
+                                                    <ActionIcon variant="transparent" ml={8} aria-label="Edit Student Name" className='cursor-pointer'>
+                                                        <IconEdit size={16}/>
+                                                    </ActionIcon>
+                                                </Flex>
+                                                <Text size='sm' c="dimmed">{submission.id}</Text>
+                                            </Stack>
+                                        ) : (
+                                            <Autocomplete
+                                                placeholder="Select student or enter name"
+                                                // data={studentsList.map((student) => ({
+                                                //     value: student.personal_data_id,
+                                                //     label: student.full_name,
+                                                //   }))}
+                                                styles={{
+                                                    option: {
+                                                    minHeight: '40px',
+                                                    },
+                                                }}
+                                                data={autocompleteData}
+                                                limit={10}
+                                                maxDropdownHeight={200}
+                                                comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
+                                            />                                        
+                                        )}
+                                    </Table.Td>
+                                    <Table.Td>
+                                        {submission.sectionsSubmitted !== '-' ? (
+                                            <Text>{submission.sectionsSubmitted}</Text>
+                                        ) : (
+                                            // <Select
+                                            //     placeholder="Select section"
+                                            //     data={['801', '802', '803']}
+                                            //     onChange={(value) => handleStudentNameChange(submission.id, value || '')}
+                                            // />
+                                            <></>
+                                        )}
+                                    </Table.Td>
+                                    <Table.Td>
+                                        <Text>{submission.submissionTime}</Text>
+                                    </Table.Td>
+                                    <Table.Td ta='center'>
+                                        <Badge w={52} color={submission.graded > 0 ? 'orange' : 'red'}>
+                                            {submission.graded}%
+                                        </Badge>
+                                    </Table.Td>
+                                    <Table.Td ta='center'>
+                                        <ActionIcon variant="transparent" aria-label="Delete Submission">
+                                            <RiDeleteBinLine size={20} />
+                                        </ActionIcon>
+                                    </Table.Td>
+                                </Table.Tr>
+                            ))}
+                        </Table.Tbody>
+                    </Table>
+                </ScrollArea>
 
-                <Flex justify="center" mt="xs">
-                    <Pagination 
-                        total={totalPages} 
-                        siblings={1}
-                        boundaries={1}
-                        value={pagination.active} 
-                        onChange={pagination.setPage} 
-                    />
+                <Flex justify="space-between" mt="xs" align="center">
+                    <Flex flex={1} justify="center">
+                        <Pagination 
+                            total={totalPages} 
+                            siblings={1}
+                            boundaries={1}
+                            value={pagination.active} 
+                            onChange={pagination.setPage} 
+                        />
+                    </Flex>
+                    <Flex align="center" justify="flex-end" gap="sm" w="auto">
+                        <Text size="sm" c="dimmed">
+                            {message}
+                        </Text>
+                        <Combobox
+                            size='sm'
+                            store={combobox}
+                            withinPortal={false}
+                            onOptionSubmit={(value) => setPageSize(Number(value))}
+                            >
+                            <Combobox.Target>
+                                <TextInput
+                                value={pageSize}
+                                onChange={(event) => setPageSize(Number(event.currentTarget.value))}
+                                rightSection={<Combobox.Chevron />}
+                                onClick={() => combobox.openDropdown()}
+                                />
+                            </Combobox.Target>
+
+                            <Combobox.Dropdown>
+                                <Combobox.Options>
+                                {['5', '10', '15'].map((size) => (
+                                    <Combobox.Option key={size} value={size}>
+                                    {size}
+                                    </Combobox.Option>
+                                ))}
+                                </Combobox.Options>
+                            </Combobox.Dropdown>
+                        </Combobox>
+                    </Flex>
                 </Flex>
             </Box>
         </Box>
