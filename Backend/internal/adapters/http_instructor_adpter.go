fd3ea24c01c759a6a4225bb58b264b80c2b38b8e
jirayu <jirayu004za@gmail.com>
2025-02-24 02:34:50 +0700

    impl http handler GetSubmissionsListForManagement

--- Backend/internal/adapters/http_instructor_adpter.go
+++ Backend/internal/adapters/http_instructor_adpter.go
@@ -1230,6 +1230,38 @@ func (h *HttpInstructorHandler) GetSubmissionFileURL(c *fiber.Ctx) error {
 	})
 }
 
+func (h *HttpInstructorHandler) GetSubmissionsListForManagement(c *fiber.Ctx) error {
+	courseIDParam := c.Query("course_id")
+	courseID, err := uuid.Parse(courseIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid course_id",
+			"error":   err.Error(),
+		})
+	}
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	submissionsList, err := h.services.GetSubmissionsListForManagement(courseID, assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get submissions list",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":     "Submissions list is retrieved",
+		"submissions": submissionsList,
+	})
+}
+
 func (h *HttpInstructorHandler) GetStudentListForSubmission(c *fiber.Ctx) error {
 	courseIDParam := c.Query("course_id")
 	courseID, err := uuid.Parse(courseIDParam)
