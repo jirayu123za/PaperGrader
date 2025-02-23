1649a64a9e3dd1d6aa60054f246a57d8184fac7d
jirayu <jirayu004za@gmail.com>
2025-02-24 02:36:28 +0700

    add submissionsList, setSubmissionsList, searchQuery, setSearchQuery, filterStatus, setFilterStatus

--- Frontend/store/ManageScan/useStudentsListStore.ts
+++ Frontend/store/ManageScan/useStudentsListStore.ts
@@ -8,9 +8,24 @@ interface StudentsList {
     has_submission: boolean;
 }
 
+interface SubmissionsList {
+    submission_id: string;
+    section_name: string;
+    full_name: string;
+    student_code: string;
+    has_assigned: boolean;
+    submitted_at: string;
+}
+
 interface StudentsListStore {
     studentsList: StudentsList[];
     setStudentsList: (studentsList: StudentsList[]) => void;
+    submissionsList: SubmissionsList[];
+    setSubmissionsList: (submissionsList: SubmissionsList[]) => void;
+    searchQuery: string;
+    setSearchQuery: (query: string) => void;
+    filterStatus: 'All' | 'true' | 'false';
+    setFilterStatus: (status: 'All' | 'true' | 'false') => void;
     pageSize: number;
     setPageSize: (size: number) => void;
 }
@@ -18,6 +33,12 @@ interface StudentsListStore {
 export const useStudentsListStore = create<StudentsListStore>((set) => ({
     studentsList: [],
     setStudentsList: (studentsList) => set({ studentsList }),
+    submissionsList: [],
+    setSubmissionsList: (submissionsList) => set({ submissionsList }),
+    searchQuery: "",
+    setSearchQuery: (query) => set({ searchQuery: query }),
+    filterStatus: "All",
+    setFilterStatus: (status) => set({ filterStatus: status }),
     pageSize: 5,
     setPageSize: (size) => set({ pageSize: size }),
 }));
