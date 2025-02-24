fc55c3f10a24ed4fb655f4af76d1a47be03ff64f
jirayu <jirayu004za@gmail.com>
2025-02-25 04:05:00 +0700

    add API PATCH UpdateSubmissionList

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -173,6 +173,7 @@ func main() {
 	apiGroup.Get("/instructor/submissionsList", instructorHandler.GetSubmissionListByCourseIDAndAssignmentID)
 	apiGroup.Get("/instructor/submission/fileURL", instructorHandler.GetSubmissionFileURL)
 	apiGroup.Get("/instructor/submission/manage", instructorHandler.GetSubmissionsListForManagement)
+	apiGroup.Patch("/instructor/submission/manage", instructorHandler.UpdateSubmissionList)
 	apiGroup.Get("/instructor/submission/studentList", instructorHandler.GetStudentListForSubmission)
 	apiGroup.Get("/instructor/submission/test", instructorHandler.GetAssignmentTemplateCount)
 
