9a03140acc65a9f00a29c777d447939d7c12639b
jirayu <jirayu004za@gmail.com>
2025-02-25 04:16:12 +0700

    impl useUpdateSubmission

---
+++ Frontend/hooks/ManageScan/useUpdateSubmission.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface updateSubmissionParams {
+    submission_id: string;
+    assignment_id: string;
+    personal_data_id: string;
+}
+
+const updateSubmission = async ({ submission_id, assignment_id, personal_data_id }: updateSubmissionParams) => {
+    const { data } = await axios.patch(`/api/api/instructor/submission/manage`, null, {
+        params: {
+            submission_id,
+            assignment_id,
+            personal_data_id,
+        },
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return data;
+};
+
+export const useUpdateSubmission = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: updateSubmission,
+        onSuccess: (data) => {
+            console.log('Update successful:', data);
+            alert(`Submission updated successfully!`);
+            queryClient.invalidateQueries({ queryKey: ['submissions'] });
+        },
+        onError: (error) => {
+            console.error('Update failed:', error);
+            alert('Failed to update the submission. Please try again.');
+        },
+    });
+};
