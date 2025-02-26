2ff30ddb55d5b80a7779066a5a17dd9222ccb313
jirayu <jirayu004za@gmail.com>
2025-02-27 00:34:46 +0700

    impl services GetSubmissionFile, CreateCroppedImage

--- Backend/internal/core/services/minio_services.go
+++ Backend/internal/core/services/minio_services.go
@@ -7,7 +7,9 @@ import (
 
 // Primary port
 type MinIOService interface {
+	GetSubmissionFile(CourseID, AssignmentID, fileName string) ([]byte, error)
 	CreateFileToMinIO(file multipart.File, CourseID, AssignmentID, fileName string) error
+	CreateCroppedImage(CourseID, AssignmentID, fileName string, fileData []byte) error
 }
 
 type MinIOServiceImpl struct {
@@ -21,8 +23,23 @@ func NewMinIOService(repo repositories.MinIORepository) MinIOService {
 	}
 }
 
+func (s *MinIOServiceImpl) GetSubmissionFile(CourseID, AssignmentID, fileName string) ([]byte, error) {
+	if file, err := s.repo.FindSubmissionFile(CourseID, AssignmentID, fileName); err != nil {
+		return nil, err
+	} else {
+		return file, nil
+	}
+}
+
 func (s *MinIOServiceImpl) CreateFileToMinIO(file multipart.File, CourseID, AssignmentID, fileName string) error {
 	if err := s.repo.AddFileToMinIO(file, CourseID, AssignmentID, fileName); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *MinIOServiceImpl) CreateCroppedImage(CourseID, AssignmentID, fileName string, fileData []byte) error {
+	if err := s.repo.AddCroppedImage(CourseID, AssignmentID, fileName, fileData); err != nil {
 		return err
 	}
 	return nil
