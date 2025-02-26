d96e17c10a569453f04e47946c988277c1bc073d
jirayu <jirayu004za@gmail.com>
2025-02-27 00:33:53 +0700

    add repo AddCroppedImage, FindSubmissionFile

--- Backend/internal/core/repositories/minio_repository.go
+++ Backend/internal/core/repositories/minio_repository.go
@@ -4,8 +4,10 @@ import "mime/multipart"
 
 type MinIORepository interface {
 	AddFileToMinIO(file multipart.File, CourseID, AssignmentID, fileName string) error
+	AddCroppedImage(CourseID, AssignmentID, fileName string, fileData []byte) error
 	FindFileFromMinIO(CourseID, AssignmentID, fileName string) (string, error)
 	FindFilesAndNames(CourseID, AssignmentID, fileNames []string) ([]string, []string, error)
+	FindSubmissionFile(CourseID, AssignmentID, fileName string) ([]byte, error)
 
 	FindTemplatePageCountFromMinIO(CourseID, AssignmentID, fileName string) (int, error)
 }
