722aaacb003709dba6f2c13fa20fc95e665eb4fd
jirayu <jirayu004za@gmail.com>
2025-04-22 13:29:29 +0700

    update isInCreateOutline

--- Frontend/app/instructor/course/[course_id]/layout.tsx
+++ Frontend/app/instructor/course/[course_id]/layout.tsx
@@ -3,17 +3,21 @@
 import { usePathname } from 'next/navigation';
 import { ReactNode } from 'react';
 import LeftMain from '@/components/LeftINS/LeftMain';
-import LeftProcess from '@/components/LeftINS/LeftProcess'; // ✅ เปลี่ยนตาม path ที่คุณเก็บไว้
+import LeftProcess from '@/components/LeftINS/LeftProcess';
+import Create from '@/components/INS/INSProcess/Right/Create';
 
 export default function Layout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
-
   const isInProcess = pathname.includes('/process/');
+  const isInCreateOutline = pathname.includes('/create-outline');
 
   return (
     <div className="flex min-h-screen">
       {isInProcess ? <LeftProcess /> : <LeftMain />}
-      <div className="flex-grow">{children}</div>
+        <div className="flex-grow">{children}</div>
+      {isInCreateOutline && (
+        <Create/>
+      )}
     </div>
   );
 }
