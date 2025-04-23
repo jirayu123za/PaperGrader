420714cb4e3a61266c28bfaa480b5cb288372897
jirayu <jirayu004za@gmail.com>
2025-04-24 01:20:54 +0700

    add metadata

--- Frontend/app/instructor/course/[course_id]/dashboard/page.tsx
+++ Frontend/app/instructor/course/[course_id]/dashboard/page.tsx
@@ -3,6 +3,11 @@ import INTDashBoard from '@/components/INS/INSDashBoard/INSDashBoard';
 import { Loader } from '@mantine/core';
 import { Suspense } from 'react';
 
+export const metadata = {
+  title: 'Instructor Dashboard',
+  description: 'Instructor Dashboard',
+};
+
 export default async function Dashboard() {
   return (
     <div className="p-6 w-full">
