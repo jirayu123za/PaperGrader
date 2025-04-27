bfbffafd3effc2b469357c720756de2f2065ab43
jirayu <jirayu004za@gmail.com>
2025-04-28 00:15:41 +0700

    update unused code, change color to c

--- Frontend/components/ManageRoster/ViewStudentList.tsx
+++ Frontend/components/ManageRoster/ViewStudentList.tsx
@@ -5,11 +5,10 @@ import { Modal, Pagination, Skeleton, Table, Text } from '@mantine/core';
 import { useFetchSectionUsersRoster } from '../../hooks/Roster/useFetchUsersRoster';
 import { useRosterStore } from '../../store/useRosterStore';
 import { useModalStore } from '../../store/modal/useRosterModalStore';
-import { useRouter , useParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { usePagination } from '@mantine/hooks';
 
 const ViewStudentLists: React.FC = () => {
-    const router = useRouter();
     const params = useParams();
     const course_id = params?.course_id as string;
     const { isOpen, selectedSection, closeModal } = useModalStore();
@@ -42,8 +41,8 @@ const ViewStudentLists: React.FC = () => {
             }}
             centered
         >
-            <Text size="sm" color="dimmed" mb="md">
-                Visit the <Text component="a" href="/roster" color="blue" inherit>roster page</Text> to make any edits to the students in this list.
+            <Text size="sm" c="dimmed" mb="md">
+                Visit the <Text component="a" href="/roster" c="blue" inherit>roster page</Text> to make any edits to the students in this list.
             </Text>
 
             {sectionUsersList && sectionUsersList.length > 0 ? (
@@ -76,7 +75,7 @@ const ViewStudentLists: React.FC = () => {
                                 </Table.Tr>
                             ))
                             : error ?
-                                <Text ta="center" color="red">Error fetching student list</Text>
+                                <Text ta="center" c="red">Error fetching student list</Text>
                                 : paginatedData.map((student, index) => (
                                     <Table.Tr key={index}>
                                         <Table.Td style={{ textAlign: 'left' }}>{student.student_code || 'N/A'}</Table.Td>
@@ -98,7 +97,7 @@ const ViewStudentLists: React.FC = () => {
                 </div>
                 </>
             ) : (
-                <Text ta="center" color="dimmed">
+                <Text ta="center" c="dimmed">
                     No students found in this section.
                 </Text>
             )}
