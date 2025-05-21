74af152c3c9ad2306320b01f0821fc24e3a286f4
jirayu <jirayu004za@gmail.com>
2025-05-21 14:50:33 +0700

    init useFetchStudentMatching

---
+++ Frontend/hooks/ManageScan/ีuseFetchStudentMatching.ts
@@ -0,0 +1,41 @@
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { useStudentMatchingStore } from "@/store/ManageScan/useStudentMatchingStore";
+import axios from "axios";
+
+interface StudentMatchingData {
+    submission_id: string;
+    is_match: boolean;
+    has_assigned: boolean;
+    personal_data_id: string | null;
+    full_name: string;
+    student_code: string;
+    section_name: string;
+    best_match_name: string;
+    best_match_id: string;
+    similarity: number;
+    submitted_at: string;
+    url_file_name: string;
+    url_file_id: string;
+}
+
+export const useFetchStudentMatching = (course_id: string, assignment_id: string, options?: UseQueryOptions) => {
+    const setStudentMatchingData = useStudentMatchingStore((state) => state.setStudentMatchingData);
+
+    return useQuery<StudentMatchingData[], Error>({
+        queryKey: ['submissions', course_id, assignment_id],
+        queryFn: async () => {
+            const response = await axios.get('/api/api/instructor/ocr/submissions', {
+                params: { course_id: course_id, assignment_id: assignment_id },
+            });
+
+            if (response.status !== 200) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = response.data.submissions;
+            setStudentMatchingData(data || []);
+            return data || [];
+        },
+        enabled: !!course_id && !!assignment_id && options?.enabled !== false,
+    });
+}
