6a92dceb0560c0e0fc5df341d443f18cf078105f
jirayu <jirayu004za@gmail.com>
2025-05-27 02:47:06 +0700

    update hook useFetchStudentsList for support update api and resp

--- Frontend/hooks/ManageScan/useFetchStudentsList.ts
+++ Frontend/hooks/ManageScan/useFetchStudentsList.ts
@@ -2,18 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 import { useStudentsListStore } from "../../store/ManageScan/useStudentsListStore";
 import axios from "axios";
 
-interface StudentsList {
+interface Student {
     personal_data_id: string;
     full_name: string;
     email: string;
     student_code: string;
-    has_submission: boolean;
+}
+
+interface StudentSubmissionSplitResponse {
+    with_submission: Student[];
+    without_submission: Student[];
 }
 
 export const useFetchStudentsList = (course_id: string, assignment_id: string) => {
-    const setStudentsList = useStudentsListStore((state) => state.setStudentsList);
+    const setWithSubmission = useStudentsListStore((state) => state.setWithSubmission);
+    const setWithoutSubmission = useStudentsListStore((state) => state.setWithoutSubmission);
 
-    return useQuery<StudentsList[], Error>({
+    return useQuery<StudentSubmissionSplitResponse, Error>({
         queryKey: ['students', course_id, assignment_id],
         queryFn: async () => {
             const response = await axios.get('/api/api/instructor/submission/studentList', {
@@ -24,9 +29,10 @@ export const useFetchStudentsList = (course_id: string, assignment_id: string) =
                 throw new Error('Network response was not ok');
             }
 
-            const data = response.data.students;
-            setStudentsList(data || []);
-            return data || [];
+            const data = response.data as StudentSubmissionSplitResponse;
+            setWithSubmission(data.with_submission || []);
+            setWithoutSubmission(data.without_submission || []);
+            return data;
         },
         enabled: !!course_id && !!assignment_id,
     });
