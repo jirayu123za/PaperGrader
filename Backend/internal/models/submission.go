b68aebcfca81376e66472c60d8bdbccb5c6337fe
jirayu <jirayu004za@gmail.com>
2025-05-26 17:27:47 +0700

    add field MatchedBy

--- Backend/internal/models/submission.go
+++ Backend/internal/models/submission.go
@@ -13,8 +13,9 @@ type Submission struct {
 	BelongsTo          uuid.NullUUID   `json:"personal_data_id"`
 	AssignmentID       uuid.UUID       `gorm:"not null" json:"assignment_id"`
 	SubmissionFileName string          `gorm:"not null" json:"submission_file_name"`
-	SubmittedAt        time.Time       `gorm:"not null" json:"submitted_at"`
+	MatchedBy          *string         `gorm:"type:submission_matched_by" json:"matched_by"`
 	SubmissionBox      []SubmissionBox `gorm:"foreignKey:SubmissionID"`
+	SubmittedAt        time.Time       `gorm:"not null" json:"submitted_at"`
 	CreatedAt          time.Time
 	UpdatedAt          time.Time
 	DeletedAt          gorm.DeletedAt `gorm:"index"`
