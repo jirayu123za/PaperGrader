36fb72ebe906e067e057138a57e99a76c6f1b619
jirayu <jirayu004za@gmail.com>
2025-05-26 17:43:54 +0700

    update repo ModifySubmissionList add parameter MatchedBy string

--- Backend/internal/core/repositories/instructor_repository.go
+++ Backend/internal/core/repositories/instructor_repository.go
@@ -44,7 +44,7 @@ type InstructorRepository interface {
 	// CRUD operations for Submissions
 	AddSubmissionFiles(submission []models.Submission) error
 	AddSubmissionAFile(submissionFile *models.Submission) error
-	ModifySubmissionList(SubmissionID uuid.UUID, AssignmentID uuid.UUID, PersonalDataID uuid.UUID) error
+	ModifySubmissionList(SubmissionID uuid.UUID, AssignmentID uuid.UUID, PersonalDataID uuid.UUID, MatchedBy string) error
 	FindSubmissionFiles(AssignmentID uuid.UUID) ([]response.SubmissionFilesResponse, error)
 	FindSubmissionListByCourseIDAndAssignmentID(CourseID uuid.UUID, AssignmentID uuid.UUID) ([]response.SubmissionResponse, error)
 	FindSubmissionFileName(AssignmentID uuid.UUID, SubmissionID uuid.UUID) (fileName string, err error)
