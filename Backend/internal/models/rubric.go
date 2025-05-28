ea089ca2b23d7cec2aa78385ab2758a38ed2cac0
jirayu <jirayu004za@gmail.com>
2025-05-28 21:58:25 +0700

    update model Rubric

--- Backend/internal/models/rubric.go
+++ Backend/internal/models/rubric.go
@@ -4,13 +4,14 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
 type Rubric struct {
-	RubricID     uuid.UUID              `gorm:"primaryKey" json:"rubric_id"`
-	AssignmentID uuid.UUID              `gorm:"not null" json:"assignment_id"`
-	RubricData   map[string]interface{} `gorm:"type:jsonb;not null" json:"rubric_data"`
+	RubricID     uuid.UUID      `gorm:"primaryKey" json:"rubric_id"`
+	AssignmentID uuid.UUID      `gorm:"not null" json:"assignment_id"`
+	RubricData   datatypes.JSON `gorm:"type:jsonb;" json:"rubric_data"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 	DeletedAt    gorm.DeletedAt `gorm:"index"`
