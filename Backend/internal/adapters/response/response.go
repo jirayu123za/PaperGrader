d51ba545b448667a7cc3d4ef3ea37a5e5795f46a
jirayu <jirayu004za@gmail.com>
2025-05-30 19:17:40 +0700

    add struct RubricData, update struct QuestionsTemplateResponse

--- Backend/internal/adapters/response/response.go
+++ Backend/internal/adapters/response/response.go
@@ -133,10 +133,15 @@ type BoundingBoxTemplateResponse struct {
 	BoundingBoxPage     uint      `json:"bounding_box_page"`
 }
 
+type RubricData struct {
+	RubricID   uuid.UUID
+	RubricData []byte
+}
+
 // Struct for Get Questions By Assignment Template
 type QuestionsTemplateResponse struct {
-	RubricID   uuid.UUID              `json:"rubric_id"`
-	RubricData map[string]interface{} `json:"rubric_data"`
+	RubricID      uuid.UUID     `json:"rubric_id"`
+	QuestionsData []interface{} `json:"questions_data"`
 }
 
 type PersonalDataIDResponse struct {
