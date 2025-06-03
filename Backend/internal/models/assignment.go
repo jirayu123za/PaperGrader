a1f98f3fb3ed083c13e7273ab863ebb60eca8cb2
jirayu <jirayu004za@gmail.com>
2025-06-04 01:33:39 +0700

    update change fields SubmittedBy, LateSubmitted, GroupSubmitted

--- Backend/internal/models/assignment.go
+++ Backend/internal/models/assignment.go
@@ -12,10 +12,10 @@ type Assignment struct {
 	CourseID              uuid.UUID           `gorm:"not null" json:"course_id"`
 	AssignmentName        string              `gorm:"type:varchar(255);not null" json:"assignment_name"`
 	AssignmentDescription string              `gorm:"type:varchar(255)" json:"assignment_description"`
-	SubmissBy             string              `gorm:"type:varchar(50);not null" json:"submiss_by"`
+	SubmittedBy           string              `gorm:"type:varchar(50);not null" json:"submitted_by"`
 	GradingType           string              `gorm:"type:varchar(50);not null;default:'negative'" json:"grading_type"`
-	LateSubmiss           bool                `gorm:"type:boolean;not null" json:"late_submiss"`
-	GroupSubmiss          bool                `gorm:"type:boolean;not null" json:"group_submiss"`
+	LateSubmitted         bool                `gorm:"type:boolean;not null" json:"late_submitted"`
+	GroupSubmitted        bool                `gorm:"type:boolean;not null" json:"group_submitted"`
 	Published             bool                `gorm:"type:boolean;not null" json:"published"`
 	Regrades              bool                `gorm:"type:boolean;not null" json:"regrades"`
 	AssignmentFiles       []AssignmentFile    `gorm:"foreignKey:AssignmentID"`
