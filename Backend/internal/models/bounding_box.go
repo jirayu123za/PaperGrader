458d33d9ba811ed3916c7706068606b33b450d37
jirayu <jirayu004za@gmail.com>
2025-06-08 19:03:21 +0700

    update model BoundingBox change BoundingBoxData as JSONB

--- Backend/internal/models/bounding_box.go
+++ Backend/internal/models/bounding_box.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
@@ -15,15 +16,22 @@ const (
 	QuestionRegion BoundingBoxType = "question"
 )
 
+type BoundingBoxData struct {
+	PointX float64         `json:"point_x"`
+	PointY float64         `json:"point_y"`
+	Width  float64         `json:"width"`
+	Height float64         `json:"height"`
+	Type   BoundingBoxType `json:"bounding_box_type"`
+	Page   uint            `json:"bounding_box_page"`
+}
+
 type BoundingBox struct {
-	BoundingBoxID       uuid.UUID       `gorm:"primaryKey" json:"bounding_box_id"`
-	AssignmentID        uuid.UUID       `gorm:"not null" json:"assignment_id"`
-	BoundingBoxPosition string          `gorm:"type:box;not null" json:"bounding_box_position"`
-	BoundingBoxType     BoundingBoxType `gorm:"type:bounding_box_type_enum;not null" json:"bounding_box_type"`
-	BoundingBoxPage     uint            `gorm:"not null" json:"bounding_box_page"`
-	CreatedAt           time.Time
-	UpdatedAt           time.Time
-	DeletedAt           gorm.DeletedAt `gorm:"index"`
+	BoundingBoxID   uuid.UUID      `gorm:"primaryKey" json:"bounding_box_id"`
+	AssignmentID    uuid.UUID      `gorm:"not null" json:"assignment_id"`
+	BoundingBoxData datatypes.JSON `gorm:"type:jsonb;not null" json:"bounding_box_data"`
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+	DeletedAt       gorm.DeletedAt `gorm:"index"`
 }
 
 func (bounding_box *BoundingBox) BeforeCreate(tx *gorm.DB) (err error) {
