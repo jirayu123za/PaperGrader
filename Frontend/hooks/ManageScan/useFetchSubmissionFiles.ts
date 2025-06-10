32f0281d3738d601665052b8d12f0b7d4f1292af
jirayu <jirayu004za@gmail.com>
2025-06-10 17:13:28 +0700

    update hook useFetchSubmissionFiles

--- Frontend/hooks/ManageScan/useFetchSubmissionFiles.ts
+++ Frontend/hooks/ManageScan/useFetchSubmissionFiles.ts
@@ -1,18 +1,20 @@
+import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 import { useSubmissionFilesStore } from '../../store/ManageScan/useSubmissionFiles';
-import axios from 'axios';
 
-interface SubmissionFiles {
+interface SubmissionsList {
     submission_id: string;
-    submission_file_name: string;
+    file_name: string;
     submitted_at: string;
+    total_submissions: number;
+    submitted_by: string;
 }
 
 export const useFetchSubmissionFiles = (assignment_id: string) => {
-    const setSubmissionFiles = useSubmissionFilesStore((state) => state.setSubmissions);
+    const setSubmissionFiles = useSubmissionFilesStore((state) => state.setSubmissionsList);
 
-    return useQuery<SubmissionFiles[], Error>({
-        queryKey: ['submissions', assignment_id],
+    return useQuery<SubmissionsList[], Error>({
+        queryKey: ['submissions_list', assignment_id],
         queryFn: async () => {
             const response = await axios.get('/api/api/instructor/submission/files', {
                 params: {
@@ -29,5 +31,6 @@ export const useFetchSubmissionFiles = (assignment_id: string) => {
             return data || [];
         },
         enabled: !!assignment_id,
+        refetchOnWindowFocus: false,
     });
 };
