81c69f0c0507de639a7f796454abc8122313e535
jirayu <jirayu004za@gmail.com>
2025-06-23 18:11:18 +0700

    feat: add NoRubric component to handle cases with no rubrics available

---
+++ Frontend/components/INS/INSProcess/Right/Rubric/NoRubric.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import React from 'react'
+import { Flex, Image, Button, Text } from '@mantine/core'
+import { FaPlus } from 'react-icons/fa'
+import { useQuestionStore } from '@/store/question/useQuestionStore'
+import { useCreateRubric } from '@/hooks/Rubric/useCreateRubric'
+
+type Props = {
+    assignment_id: string;
+}
+
+export const NoRubric = ({ assignment_id }: Props) => {
+  const { selectedQuestion, defaultSelectedQuestion } = useQuestionStore();
+  const { mutate: createRubric, isPending } = useCreateRubric(assignment_id);
+  const target = selectedQuestion ?? defaultSelectedQuestion;
+
+  const handleCreateRubric = () => {
+    createRubric({ 
+        assignment_id, 
+        question_id: target?.question_id,
+        sub_question_id: target?.sub_question_id,
+        rubric: {
+            rubric_setting: "Negative scoring",
+            rubric_details: [{
+                rubric_point: 0,
+                rubric_description: "",
+            }],
+        },
+    });
+  };
+
+  return (
+    <Flex direction="column" align="center" justify="center" gap="xs" py="xl" w='100%'>
+        <Image
+            src="/Image/table/no_data.svg"
+            alt="No rubrics found"
+            w="auto"
+            h={150}
+            fit="contain"
+            fallbackSrc="https://placehold.co/200x200?text=Placeholder"
+        />
+        <Text size="lg" fw={500} mt="md">
+            No rubrics found
+        </Text>
+        <Text size="sm" c="dimmed">
+            You haven’t created any rubrics yet.
+        </Text>
+
+        <Button
+            leftSection={<FaPlus size={12} />}
+            w={456}
+            variant="outline"
+            color="violet"
+            className="mt-2"
+            onClick={handleCreateRubric}
+            loading={isPending}
+        >
+            Add rubric item
+        </Button>
+    </Flex>
+  )
+}
