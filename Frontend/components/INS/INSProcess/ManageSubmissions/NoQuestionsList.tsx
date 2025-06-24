515b973768249e7d406b28beddaee6aef595fbcd
jirayu <jirayu004za@gmail.com>
2025-06-24 22:34:50 +0700

    feat: add NoQuestionsList component to handle empty question states

---
+++ Frontend/components/INS/INSProcess/ManageSubmissions/NoQuestionsList.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import React from 'react'
+import { Flex, Text, Image, Button } from '@mantine/core'
+import { useParams, useRouter } from 'next/navigation'
+import { IoChevronBackOutline } from "react-icons/io5";
+
+export const NoQuestionsList = () => {
+    const router = useRouter();
+    const { course_id, assignment_id } = useParams() as { course_id: string; assignment_id: string };
+    const handleBack = () => {
+        router.push(`/instructor/course/${course_id}/process/${assignment_id}/create-outline`)
+    }
+
+    return (
+        <Flex direction="column" align="center" justify="center" gap="xs" py="xl" w='100%' h="80vh">
+            <Image
+                src="/Image/table/add_notes.svg"
+                alt="No question"
+                w="auto"
+                h={200}
+                fit="contain"
+                fallbackSrc="https://placehold.co/200x200?text=Placeholder"
+            />
+            <Text size="lg" fw={500} mt="md">
+                No question found
+            </Text>
+            <Text size="sm" c="dimmed">
+                Please create bounding box for question to view or edit rubrics.
+            </Text>
+            <Button mt="lg" onClick={handleBack} leftSection={<IoChevronBackOutline />} variant="light" color="blue">
+                Back to create
+            </Button>
+        </Flex>
+    )
+}
