7188ced1c636b9a4794fcbfc1647cdba143e5187
pulom999 <pulom999@gmail.com>
2025-06-25 18:37:52 +0700

    del   bounding_box_position add 4 type x y width height

--- Frontend/components/INS/INSProcess/Right/Boundingbox/boundingBoxActions.ts
+++ Frontend/components/INS/INSProcess/Right/Boundingbox/boundingBoxActions.ts
@@ -1,52 +1,50 @@
-
-
 import { nanoid } from 'nanoid';
 import useBoundingBoxStore from '@/store/BoundingBox/useBoundingBoxStore';
 
 export function handleAddNameBoundingBox() {
-  const { boundingBoxes, addBoundingBox } = useBoundingBoxStore.getState();
-  const hasName = boundingBoxes.some(b => b.bounding_box_type === 'name');
-  if (hasName) return;
+  const { addBoundingBox } = useBoundingBoxStore.getState();
   addBoundingBox({
     bounding_box_id: nanoid(),
-    bounding_box_position: '100,100,200,50',
     bounding_box_type: 'name',
     bounding_box_page: 1,
+    point_x: 50,
+    point_y: 100,
+    width: 200,
+    height: 50,
   });
-};
+}
 
 export function handleAddIdBoundingBox() {
-  const { boundingBoxes, addBoundingBox } = useBoundingBoxStore.getState();
-  const hasId = boundingBoxes.some(b => b.bounding_box_type === 'id');
-  if (hasId) return;
+  const { addBoundingBox } = useBoundingBoxStore.getState();
   addBoundingBox({
     bounding_box_id: nanoid(),
-    bounding_box_position: '100,200,200,50',
     bounding_box_type: 'id',
     bounding_box_page: 1,
+    point_x: 50,
+    point_y: 160,
+    width: 200,
+    height: 50,
   });
-};
+}
 
 export function handleAddQuestionAndBoundingBox() {
-  const {
-    addBoundingBox,
-    addQuestion,
-  } = useBoundingBoxStore.getState();
-
-  const boundingBoxId = nanoid(); 
-
+  const { addBoundingBox, addQuestion } = useBoundingBoxStore.getState();
+  const bounding_box_id = nanoid();
+  const question_id = nanoid();
 
   addBoundingBox({
-    bounding_box_id: boundingBoxId,
+    bounding_box_id,
     bounding_box_type: 'question',
-    bounding_box_position: '100,200,200,50',
-    bounding_box_page: 0
+    bounding_box_page: 1,
+    point_x: 100,
+    point_y: 200,
+    width: 300,
+    height: 100,
   });
 
-
   addQuestion({
-    question_id: nanoid(),
-    bounding_box_id: boundingBoxId, 
+    question_id,
+    bounding_box_id,
     question_title: 'New Question',
     question_point: 0,
     subquestions: [],
