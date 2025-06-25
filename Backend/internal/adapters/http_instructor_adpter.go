fd6331b0d5694f8e233414f4e6c8240f330835f2
jirayu <jirayu004za@gmail.com>
2025-06-26 03:51:33 +0700

    add GetSubmissionsFromQuestion handler to retrieve submissions for a specific course and assignment

--- Backend/internal/adapters/http_instructor_adpter.go
+++ Backend/internal/adapters/http_instructor_adpter.go
@@ -2006,3 +2006,37 @@ func (h *HttpInstructorHandler) MockGetData(c *fiber.Ctx) error {
 		"message":        "Assignment template data retrieved",
 	})
 }
+
+// Question Handlers
+func (h *HttpInstructorHandler) GetSubmissionsFromQuestion(c *fiber.Ctx) error {
+	courseIDParam := c.Query("course_id")
+	courseID, err := uuid.Parse(courseIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid course_id",
+			"error":   err.Error(),
+		})
+	}
+
+	assignmentIDParam := c.Query("assignment_id")
+	assignmentID, err := uuid.Parse(assignmentIDParam)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid assignment_id",
+			"error":   err.Error(),
+		})
+	}
+
+	submissions, err := h.services.GetSubmissionsFromQuestion(courseID, assignmentID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get submissions from question",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":          "Submissions from question are retrieved",
+		"submissions_list": submissions,
+	})
+}
