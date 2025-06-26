4d05305faf9a5c1933006f4b609bc86cdb465b37
jirayu <jirayu004za@gmail.com>
2025-06-27 03:07:52 +0700

    feat: replace CourseRoster and ManageSection with ManageRosterClient for simplified roster management

--- Frontend/app/instructor/course/[course_id]/manageroster/page.tsx
+++ Frontend/app/instructor/course/[course_id]/manageroster/page.tsx
@@ -1,26 +1,15 @@
-"use client";
 
 import React from 'react';
-import CourseRoster from '@/components/ManageRoster/CourseRoster';
-import ManageSection from '@/components/ManageRoster/ManageSection';
-import { Tabs } from '@mantine/core';
+import { ManageRosterClient } from '@/components/client/ManageRosterClient';
+
+export const metadata = {
+  title: 'Manage Roster',
+  description: 'Manage the course roster and sections.',
+};
 
 const ManageRoster: React.FC = () => {
   return (
-    <div className="grow p-6">
-      <Tabs defaultValue="roster">
-        <Tabs.List>
-          <Tabs.Tab value="roster">Manage roster</Tabs.Tab>
-          <Tabs.Tab value="section">Manage section</Tabs.Tab>
-        </Tabs.List>
-        <Tabs.Panel value="roster">
-          <CourseRoster />
-        </Tabs.Panel>
-        <Tabs.Panel value="section">
-          <ManageSection />
-        </Tabs.Panel>
-      </Tabs>
-    </div>
+    <ManageRosterClient/>
   );
 };
 
