73fc49b37906312a2c52de43d6e2d0bbc1c1bddf
jirayu <jirayu004za@gmail.com>
2025-06-29 04:22:24 +0700

    fix: update instructor assignment route to retrieve questions list

--- Backend/internal/routes/route.go
+++ Backend/internal/routes/route.go
@@ -118,7 +118,7 @@ func RegisterRoutes(
 
 	// Template(outline)
 	apiGroup.Get("/instructor/assignment/template", instructorHandler.GetAssignmentTemplateData)
-	apiGroup.Get("/instructor/assignment/mock", instructorHandler.MockGetData)
+	apiGroup.Get("/instructor/assignment/questions", instructorHandler.GetQuestionsList)
 	// Part:1 Rubric
 	apiGroup.Post("/instructor/rubric", instructorHandler.CreateRubric)
 	apiGroup.Put("/instructor/rubric", instructorHandler.UpdateRubric)
