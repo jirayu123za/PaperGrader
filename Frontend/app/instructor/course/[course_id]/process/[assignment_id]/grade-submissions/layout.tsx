79c197df89a9b4ca425782eb236e2f32dbcfc51f
jirayu <jirayu004za@gmail.com>
2025-06-30 00:24:08 +0700

    refactor: remove padding from main content in SubmissionsLayout

--- Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/layout.tsx
+++ Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SubmissionsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
-        <main className="grow p-4">{children}</main>
+        <main className="grow">{children}</main>
     </div>
   )
 }
