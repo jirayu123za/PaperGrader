5a2cdc090f0f3b4bf23004b21f5d2cd77810545e
jirayu <jirayu004za@gmail.com>
2025-06-30 00:25:41 +0700

    refactor: remove content from RubricGrader component

--- Frontend/components/INS/INSProcess/Right/Grade/RubricGrader.tsx
+++ Frontend/components/INS/INSProcess/Right/Grade/RubricGrader.tsx
@@ -4,10 +4,6 @@ import React from 'react'
 
 export const RubricGrader = () => {
   return (
-    <Card c="grape">
-        <h1 className='text-2xl font-bold'>Rubric Grader</h1>
-        <p>This is the Rubric Grader component.</p>
-        {/* Add your rubric grading logic here */}
-    </Card>
+    <></>
   )
 }
