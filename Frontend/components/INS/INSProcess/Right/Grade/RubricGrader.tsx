46ec300d125bd631c8e7371253b328244212b17a
jirayu <jirayu004za@gmail.com>
2025-06-30 00:12:04 +0700

    feat: add RubricGrader component and integrate it into SubmissionGrader

---
+++ Frontend/components/INS/INSProcess/Right/Grade/RubricGrader.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { Card } from '@mantine/core'
+import React from 'react'
+
+export const RubricGrader = () => {
+  return (
+    <Card c="grape">
+        <h1 className='text-2xl font-bold'>Rubric Grader</h1>
+        <p>This is the Rubric Grader component.</p>
+        {/* Add your rubric grading logic here */}
+    </Card>
+  )
+}
