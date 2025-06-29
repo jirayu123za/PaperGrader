b40e47d8460d55a3489b3ed6f573e9bb18150466
pulom999 <pulom999@gmail.com>
2025-06-30 04:21:02 +0700

    เพิ่ม การระบุหน้า

--- Frontend/store/BoundingBox/usePageMetaStore.ts
+++ Frontend/store/BoundingBox/usePageMetaStore.ts
@@ -13,14 +13,15 @@ interface PageMetaStore {
   pageMetas: PageMetadata[];
   setPageMetas: (metas: PageMetadata[]) => void;
   getPageMetaByPage: (page: number) => PageMetadata | undefined;
+
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
 }
 
 export const usePageMetaStore = create<PageMetaStore>((set, get) => ({
   pageMetas: [],
-
+  currentPage: 1,
   setPageMetas: (metas) => set({ pageMetas: metas }),
-
-  getPageMetaByPage: (page) => {
-    return get().pageMetas.find((m) => m.pageNumber === page);
-  },
+  setCurrentPage: (page) => set({ currentPage: page }),
+  getPageMetaByPage: (page) => get().pageMetas.find(m => m.pageNumber === page),
 }));
