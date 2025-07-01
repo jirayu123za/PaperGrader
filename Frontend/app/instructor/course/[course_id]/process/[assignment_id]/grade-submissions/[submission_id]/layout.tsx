6b5040116e9d474e70fa8cd8c59bbd531588056f
jirayu <jirayu004za@gmail.com>
2025-07-01 23:36:29 +0700

    fix: replace RubricGrader with SidebarQuestions in GradeLayout for improved component structure

--- Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/[submission_id]/layout.tsx
+++ Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/[submission_id]/layout.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { RubricGrader } from "@/components/INS/INSProcess/Right/Grade/RubricGrader";
+import { SidebarQuestions } from "@/components/INS/INSProcess/Right/Grade/SidebarQuestions";
 
 export default function GradeLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-full min-h-screen">
       <main className="grow overflow-hidden">{children}</main>
-      {/* <Create /> */}
-      <RubricGrader />
+      <SidebarQuestions />
     </div>
   );
 }
