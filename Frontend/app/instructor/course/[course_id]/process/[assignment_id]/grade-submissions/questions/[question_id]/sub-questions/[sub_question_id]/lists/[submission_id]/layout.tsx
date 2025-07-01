659ae3c35b393806135c881e0f1dd0322b4dd082
jirayu <jirayu004za@gmail.com>
2025-07-01 22:56:14 +0700

    fix: remove unused Create component import in SubmissionLayout for cleaner code

--- Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/questions/[question_id]/sub-questions/[sub_question_id]/lists/[submission_id]/layout.tsx
+++ Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/questions/[question_id]/sub-questions/[sub_question_id]/lists/[submission_id]/layout.tsx
@@ -1,12 +1,10 @@
-import Create from '@/components/INS/INSProcess/Right/Create'
-import { RubricGrader } from '@/components/INS/INSProcess/Right/Grade/RubricGrader'
 import React from 'react'
+import { RubricGrader } from '@/components/INS/INSProcess/Right/Grade/RubricGrader'
 
 export default function SubmissionLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-full min-h-screen">
       <main className="grow overflow-hidden">{children}</main>
-      {/* <Create /> */}
       <RubricGrader />
     </div>
   )
