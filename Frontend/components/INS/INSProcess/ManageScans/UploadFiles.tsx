f80a26e5cbc0be0b1e1018f054deb478538a566c
jirayu <jirayu004za@gmail.com>
2025-07-03 19:49:01 +0700

    Update hide  DropFilesBox and Alert with condition

--- Frontend/components/INS/INSProcess/ManageScans/UploadFiles.tsx
+++ Frontend/components/INS/INSProcess/ManageScans/UploadFiles.tsx
@@ -4,6 +4,7 @@ import { Flex } from '@mantine/core';
 import { SubmissionsListTable } from './SubmissionsListTable';
 import { AlertForSubmission } from './AlertForSubmission';
 import { DropFilesBox } from './DropFilesBox';
+import { useSubmissionFilesStore } from '@/store/ManageScan/useSubmissionFiles';
 
 type Props = {
   course_id: string;
@@ -11,16 +12,20 @@ type Props = {
 };
 
 const UploadFiles: React.FC<Props> = ({ course_id, assignment_id }) => {
+  const { submissionsList } = useSubmissionFilesStore();
+
   return (
     <Flex direction="column" gap="md">
       {/* Top: Submissions Table */}
       <SubmissionsListTable assignment_id={assignment_id} />
 
       {/* Bottom: Drop zone and alert aligned vertically */}
-      <Flex direction="row" gap="md">
-        <DropFilesBox course_id={course_id} assignment_id={assignment_id} />
-        <AlertForSubmission />
-      </Flex>
+      {submissionsList.length !== 0 && (
+        <Flex direction="row" gap="md">
+          <DropFilesBox course_id={course_id} assignment_id={assignment_id} />
+          <AlertForSubmission />
+        </Flex>
+      )}
     </Flex>
   );
 };
