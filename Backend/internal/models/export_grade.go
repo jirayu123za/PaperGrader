e8db338615246572bddc19de5fb52d8010e04d12
jirayu <jirayu004za@gmail.com>
2025-07-24 14:16:38 +0700

    add: define FileStatusEnum and integrate it into ExportGrade model

--- Backend/internal/models/export_grade.go
+++ Backend/internal/models/export_grade.go
@@ -7,12 +7,23 @@ import (
 	"gorm.io/gorm"
 )
 
+type FileStatusEnum string
+
+const (
+	FileStatusPending   FileStatusEnum = "pending"
+	FileStatusCompleted FileStatusEnum = "completed"
+	FileStatusFailed    FileStatusEnum = "failed"
+)
+
 type ExportGrade struct {
-	ExportGradeID  uuid.UUID `gorm:"primaryKey"`
-	CourseID       uuid.UUID `gorm:"not null" json:"course_id"`
-	AssignmentID   uuid.UUID `gorm:"not null" json:"assignment_id"`
-	PersonalDataID uuid.UUID `gorm:"not null" json:"personal_data_id"`
-	FileName       string    `gorm:"type:varchar(255);not null" json:"file_name"`
+	ExportGradeID  uuid.UUID      `gorm:"primaryKey"`
+	CourseID       uuid.UUID      `gorm:"not null" json:"course_id"`
+	AssignmentID   uuid.UUID      `gorm:"not null" json:"assignment_id"`
+	PersonalDataID uuid.UUID      `gorm:"not null" json:"personal_data_id"`
+	FileName       string         `gorm:"type:varchar(255);not null" json:"file_name"`
+	FileStatus     FileStatusEnum `gorm:"type:file_status_enum;not null;index" json:"file_status"`
+	FileURL        string         `gorm:"type:varchar(255);not null" json:"file_url"`
+	ProcessedAt    *time.Time     `json:"processed_at,omitempty"`
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
 	DeletedAt      gorm.DeletedAt `gorm:"index"`
