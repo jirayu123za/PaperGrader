cc101b71c871840234b092cdfefd21805972cb44
jirayu <jirayu004za@gmail.com>
2025-07-28 14:50:23 +0700

    fix: update redirect URL for student overview in Google callback

--- Backend/internal/adapters/http_oauth_adpter.go
+++ Backend/internal/adapters/http_oauth_adpter.go
@@ -172,7 +172,7 @@ func (h *HttpOAuthHandler) VerifyGoogleCallback(c *fiber.Ctx) error {
 	case 1:
 		return c.Redirect("http://localhost:5173/INSCourseOverview", fiber.StatusTemporaryRedirect)
 	case 2:
-		return c.Redirect("http://localhost:5173/student/overview", fiber.StatusTemporaryRedirect)
+		return c.Redirect("http://localhost:5173/student/overview/course", fiber.StatusTemporaryRedirect)
 	default:
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "Invalid group ID",
