04420c291c9fdebc5ab7ef969da59c852f88f9c0
Kisaru1523 <76036259+Kisaru1523@users.noreply.github.com>
2025-08-02 17:37:57 +0700

    compare button&background&logo

--- Frontend/components/STD/SideBar/LeftMain.tsx
+++ Frontend/components/STD/SideBar/LeftMain.tsx
@@ -1,68 +1,90 @@
 "use client";
 
-import react from 'react';
-import { useRouter } from 'next/navigation';
-import { FaBars, FaHome, FaBook, FaCog ,FaUser} from 'react-icons/fa';
-import { useDisclosure } from '@mantine/hooks';
-import { Button, Divider, Flex, Image, Stack } from '@mantine/core';
-import AccountMenu from '../../Account';
+import react from "react";
+import { useRouter } from "next/navigation";
+import {FaHome, FaBook, FaCog, FaUser,FaRegArrowAltCircleRight } from "react-icons/fa";
+import { useDisclosure } from "@mantine/hooks";
+import { Button, Divider, Flex, Image, Stack } from "@mantine/core";
+import AccountMenu from "../../Account";
 
 export default function LeftMain() {
   const router = useRouter();
-  const [isCollapsed, { toggle }] = useDisclosure(false);
+  const [isCollapsed, { toggle:toggleCollapse }] = useDisclosure(false);
 
   const icons = {
     home: <FaHome />,
     book: <FaBook />,
     cog: <FaCog />,
     user: <FaUser />,
-  }
+  };
 
   return (
-    <div className={`relative flex flex-col justify-between border-r transition-all duration-300 ${isCollapsed ? 'w-16' : 'w-64'} h-screen`}>
-      <Flex justify="space-between" align="center" p={12}
+    <div
+      className={`relative flex flex-col justify-between border-r transition-all duration-300 ${
+        isCollapsed ? "w-16" : "w-64"
+      } h-screen`}
+    >
+      <Flex
+        justify="space-between"
+        align="center"
+        p={12}
         style={{
-        backgroundColor: '#f1f3f8',
+          backgroundColor: "#6665AC",
         }}
       >
         {/* Header and Course Name */}
-          {!isCollapsed && (
-            <Image
-              src="/Image/logo-ppgd.png"
-              alt="logo" w={200} h={60} p={2} 
-              style={{ cursor: 'pointer' }}
-              onClick={() => router.push(`/student/overview`)}
-            />
-          )}          
-          <Button
-            onClick={toggle}
-            unstyled 
-            className="bg-transparent p-2 shadow-none hover:bg-gray-200"
-          >
-            <FaBars
-              size={24}
-              className={`text-black transition-transform duration-300 ${isCollapsed ? '' : 'transform rotate-180'}`}
-            />
-          </Button>
+        {!isCollapsed && (
+          <Image
+            src="/Image/logo-ppgd2.png"
+            alt="logo"
+            w={200}
+            h={60}
+            p={2}
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              router.push(`/student/overview`);
+            }}
+          />
+        )}
+        <Button
+                  onClick={toggleCollapse}
+                  variant="transparent"
+                  radius="md"
+                  styles={() => ({
+                    root: {
+                      border: "none",
+                      padding: isCollapsed ? "0 0 0 8px" : "0",
+                      height: "auto",
+                    },
+                  })}
+                >
+                  <FaRegArrowAltCircleRight
+                    size={24}
+                    style={{
+                      color: isCollapsed ? "#f1f3f8" : "#f1f3f8",
+                    }}
+                    className={`transition-transform duration-300 ${
+                      isCollapsed ? "" : "transform rotate-180"
+                    }`}
+                  />
+                </Button>
       </Flex>
-
-      <Divider />
 
       {/* Main Menu */}
       <Stack
-        p={16} gap="xs"
-        className='grow'
+        p={16}
+        gap="xs"
+        className="grow"
         style={() => ({
-          backgroundColor: '#6665AC',
+          backgroundColor: "#6665AC",
         })}
       >
-
         <Button
           // disabled={!course}
           leftSection={icons.home}
-          variant='subtle'
+          variant="subtle"
           style={() => ({
-            color: '#F9F9F9',
+            color: "#F9F9F9",
             display: "flex",
             justifyContent: isCollapsed ? "center" : "flex-start",
           })}
@@ -76,9 +98,9 @@ export default function LeftMain() {
         <Button
           // disabled={!course}
           leftSection={icons.book}
-          variant='subtle'
+          variant="subtle"
           style={() => ({
-            color: '#F9F9F9',
+            color: "#F9F9F9",
             display: "flex",
             justifyContent: isCollapsed ? "center" : "flex-start",
           })}
@@ -92,25 +114,25 @@ export default function LeftMain() {
         <Button
           // disabled={!course}
           leftSection={icons.cog}
-          variant='subtle'
+          variant="subtle"
           style={() => ({
-            color: '#F9F9F9',
+            color: "#F9F9F9",
             display: "flex",
             justifyContent: isCollapsed ? "center" : "flex-start",
           })}
           onClick={() => {
             // router.push(`/student/${studentId}/settings`);
-            console.log('Settings');
+            console.log("Settings");
           }}
         >
           {!isCollapsed && <span>Settings</span>}
         </Button>
       </Stack>
-    
+
       {/* Account Section */}
       <Stack pb={0.75}>
         <AccountMenu isCollapsed={isCollapsed} />
       </Stack>
-  </div>
+    </div>
   );
 }
