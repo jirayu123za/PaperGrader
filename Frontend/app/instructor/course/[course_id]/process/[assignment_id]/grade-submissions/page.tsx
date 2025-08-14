6ed5e9fc82d5b6f8fb9df3202c70d19850eb4c3e
jirayu <jirayu004za@gmail.com>
2025-08-15 03:10:52 +0700

    refactor: reorder imports and simplify Submissions function

--- Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/page.tsx
+++ Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/page.tsx
@@ -1,5 +1,5 @@
-import { TabsGradeSubmissionsClient } from '@/components/client/TabsGradeSubmissionsClient';
 import React, { Suspense } from 'react'
+import { TabsGradeSubmissionsClient } from '@/components/client/TabsGradeSubmissionsClient';
 import { Loader } from '@mantine/core';
 
 export const metadata = {
@@ -7,11 +7,7 @@ export const metadata = {
   description: 'Grade Submissions for the assignment in the course process page.',
 };
 
-export default async function Submissions({ params }: { 
-  params: Promise<{ course_id: string; assignment_id: string }>
-}) {
-  const { course_id, assignment_id } = await params;
-
+export default async function Submissions() {
   return (
     <>
       <Suspense fallback={<Loader size="lg" />}>
