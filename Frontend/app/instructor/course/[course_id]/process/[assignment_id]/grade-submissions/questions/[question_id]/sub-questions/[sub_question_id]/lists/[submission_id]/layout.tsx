4eb9efe94d0a304b41b471ef88d58739abb09feb
pulom999 <pulom999@gmail.com>
2025-09-01 01:05:30 +0700

    change layout GradeBottomBar

--- Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/questions/[question_id]/sub-questions/[sub_question_id]/lists/[submission_id]/layout.tsx
+++ Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/questions/[question_id]/sub-questions/[sub_question_id]/lists/[submission_id]/layout.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import { RubricGrader } from '@/components/INS/INSProcess/Right/Grade/RubricGrader'
+import React from 'react';
+import { RubricGrader } from '@/components/INS/INSProcess/Right/Grade/RubricGrader';
+import GradeBottomBar from '@/components/INS/INSProcess/Bottombar/GradeBottomBar';
 
 export default function SubmissionLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-full min-h-screen">
+
       <main className="grow overflow-hidden">{children}</main>
       <RubricGrader />
+      <footer
+        className="fixed bottom-0 left-64 w-[calc(100%-16rem)] border-t border-gray-300 bg-white z-50"
+        style={{ padding: '0.1rem' }}
+      >
+        <GradeBottomBar />
+      </footer>
     </div>
-  )
+  );
 }
--- Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/questions/[question_id]/sub-questions/layout.tsx
+++ Frontend/app/instructor/course/[course_id]/process/[assignment_id]/grade-submissions/questions/[question_id]/sub-questions/layout.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import GradeBottomBar from "@/components/INS/INSProcess/Bottombar/GradeBottomBar";
+
 
 
 export default function SubmissionsLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
         <main>{children}</main>
-        <footer
-        className="fixed bottom-0 left-64 w-[calc(100%-16rem)] border-t border-gray-300 bg-white z-50"
-        style={{
-          padding: "0.1rem",
-        }}
-      >
-        <GradeBottomBar />
-      </footer>
     </>
   )
 }
