4996adbf128465d613577c47f45c486885e0400e
jirayu <jirayu004za@gmail.com>
2025-09-04 04:48:57 +0700

    Add hook for updating assignment time settings

---
+++ Frontend/hooks/AssignmentSetting/updateAssignmentTimeSettings.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+const updateAssignmentTimeSettings = async ({ timeData, course_id, assignment_id }: { timeData: FormData; course_id: string; assignment_id: string }) => {
+    const { data: timeSettingResponse } = await axios.put(`/api/api/instructor/assignment/time`,
+        timeData, {
+        params: { course_id, assignment_id },
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        }
+    });
+    return timeSettingResponse;
+}
+
+export const useUpdateAssignmentTimeSettings = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: updateAssignmentTimeSettings,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['ins_assignments'] });
+        },
+        onError: (error: any) => {
+        }
+    });
+}
