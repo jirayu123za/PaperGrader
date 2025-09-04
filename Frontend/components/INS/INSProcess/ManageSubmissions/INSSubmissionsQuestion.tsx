a469b8a678edec8d5fd29e747de9be91dbdad952
jirayu <jirayu004za@gmail.com>
2025-09-04 23:17:42 +0700

    Refactor INSSubmissionsQuestion component for improved readability and layout consistency

--- Frontend/components/INS/INSProcess/ManageSubmissions/INSSubmissionsQuestion.tsx
+++ Frontend/components/INS/INSProcess/ManageSubmissions/INSSubmissionsQuestion.tsx
@@ -34,168 +34,170 @@ export const INSSubmissionsQuestion: React.FC = () => {
         <Text size="lg" fw={500}>
           Grading Dashboard
         </Text>
-        <Text size="sm" c="dimmed" mb="md">
+        <Text size="sm" c="dimmed">
           Grade submissions by selecting individual questions below.
         </Text>
       </Flex>
 
       <Paper withBorder>
-        <Table highlightOnHover verticalSpacing="md" horizontalSpacing="lg">
-          <Table.Thead className='bg-gray-100'>
-            <Table.Tr>
-              <Table.Th>Question</Table.Th>
-              <Table.Th ta='center'>Points</Table.Th>
-              <Table.Th ta='center'>Progress</Table.Th>
-              <Table.Th ta='center'>Graded by</Table.Th>
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>
-            {questions.map((question, index) => {
-              const hasSub = question.sub_questions && question.sub_questions.length > 0;
+        <Table.ScrollContainer minWidth="100%" maxHeight={610} className='no-scroll-padding'>        
+          <Table highlightOnHover verticalSpacing="md" horizontalSpacing="lg">
+            <Table.Thead className='bg-gray-100'>
+              <Table.Tr>
+                <Table.Th>Question</Table.Th>
+                <Table.Th ta='center'>Points</Table.Th>
+                <Table.Th ta='center'>Progress</Table.Th>
+                <Table.Th ta='center'>Graded by</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+              {questions.map((question, index) => {
+                const hasSub = question.sub_questions && question.sub_questions.length > 0;
 
-              return (
-                <Table.Tr key={question.question_id}>
-                  {/* Question Column */}
-                  <Table.Td className="align-top">
-                    {hasSub ? (
-                      <Box className="mb-2">
-                        <Text size="md">
-                          {index + 1}: {question.question_title}
-                        </Text>
-                      </Box>
-                    ) : (
-                      <Box className="mb-2 group flex items-center gap-1 relative w-fit">
-                        <Anchor
-                          underline="hover"
-                          c="dark"
-                          onClick={() => router.push(`/instructor/course/${course_id}/process/${assignment_id}/grade-submissions/questions/${question.question_id}/submissions/${question.submission_id}`)}
-                        >
-                          {index + 1}: {question.question_title}
-                        </Anchor>
-                        <Anchor
-                          underline="hover"
-                          size='xs'
-                          ml='xs'
-                          className="invisible group-hover:visible"
-                          onClick={() => router.push(`/instructor/course/${course_id}/process/${assignment_id}/grade-submissions/questions/${question.question_id}/submissions`)}
-                        >
-                          <Flex align="center" gap={4} >
-                            <VscListUnordered size={12} className='translate-y-[1px]'/>
-                            <span>Submissions</span>
-                          </Flex>
-                        </Anchor>
-                      </Box>
-                    )}
-
-                    {hasSub && (
-                      <Flex direction="column" gap={16}>
-                        {question.sub_questions?.map((sub, subIndex) => (
-                          <Flex key={sub.sub_question_id} align="center" ml="lg" className="group relative w-fit">
-                            <span className="w-3 h-3 border-l-2 border-b-2 border-dotted border-gray-300 mr-2 -translate-y-[2px]" />
-                            <Anchor
-                              underline="hover"
-                              size="sm"
-                              c="dimmed"
-                              onClick={() => router.push(`/instructor/course/${course_id}/process/${assignment_id}/grade-submissions/questions/${question.question_id}/sub-questions/${sub.sub_question_id}/submissions/${sub.submission_id}`)}
-                            >
-                              {index + 1}.{subIndex + 1}: {sub.sub_question_title}
-                            </Anchor>
-                            <Anchor
-                              underline="hover"
-                              size='xs'
-                              ml='xs'
-                              className="invisible group-hover:visible"
-                              onClick={() => router.push(`/instructor/course/${course_id}/process/${assignment_id}/grade-submissions/questions/${question.question_id}/sub-questions/${sub.sub_question_id}/submissions`)}
-                            >
-                              <Flex align="center" gap={4} >
-                                <VscListUnordered size={12} className='translate-y-[1px]'/>
-                                <span>Submissions</span>
-                              </Flex>
-                            </Anchor>
-                          </Flex>
-                        ))}
-                      </Flex>
-                    )}
-                  </Table.Td>
-
-                  {/* Points Column */}
-                  <Table.Td ta="center" className="align-top">
-                    <Text mb="xs" size="md" fw={500} h='24.80px' c="dimmed">{question.question_point.toFixed(1)}</Text>
-                    {hasSub && (
-                      <Flex direction="column" gap={16}>
-                        {question.sub_questions?.map((sub) => (
-                          <Text
-                            key={sub.sub_question_id}
-                            size="sm"
-                            fw={500}
-                            c="dimmed"
-                          >
-                            {sub.sub_question_point.toFixed(1)}
+                return (
+                  <Table.Tr key={question.question_id}>
+                    {/* Question Column */}
+                    <Table.Td className="align-top">
+                      {hasSub ? (
+                        <Box className="mb-2">
+                          <Text size="md">
+                            {index + 1}: {question.question_title}
                           </Text>
-                        ))}
-                      </Flex>
-                    )}
-                  </Table.Td>
+                        </Box>
+                      ) : (
+                        <Box className="mb-2 group flex items-center gap-1 relative w-fit">
+                          <Anchor
+                            underline="hover"
+                            c="dark"
+                            onClick={() => router.push(`/instructor/course/${course_id}/process/${assignment_id}/grade-submissions/questions/${question.question_id}/submissions/${question.submission_id}`)}
+                          >
+                            {index + 1}: {question.question_title}
+                          </Anchor>
+                          <Anchor
+                            underline="hover"
+                            size='xs'
+                            ml='xs'
+                            className="invisible group-hover:visible"
+                            onClick={() => router.push(`/instructor/course/${course_id}/process/${assignment_id}/grade-submissions/questions/${question.question_id}/submissions`)}
+                          >
+                            <Flex align="center" gap={4} >
+                              <VscListUnordered size={12} className='translate-y-[1px]'/>
+                              <span>Submissions</span>
+                            </Flex>
+                          </Anchor>
+                        </Box>
+                      )}
 
-                  {/* Progress Column */}
-                  <Table.Td ta="center" className="align-top">
-                    {!hasSub ? (
-                      <Flex align="center" justify="center">
-                        <Progress value={question.progress ?? 0} color="green" w={350} size="lg" />
-                        <Text size="sm" fw={500} c="green" w={30}>{question.progress}%</Text>
-                      </Flex>
-                    ) : (
-                      <>
-                        <div className="min-h-[24px] mb-[10px]" />
+                      {hasSub && (
                         <Flex direction="column" gap={16}>
-                          {question.sub_questions?.map((sub) => (
-                            <Flex
-                              key={sub.sub_question_id}
-                              align="center"
-                              justify="center"
-                              gap="2px"
-                            >
-                              <Progress
-                                value={sub.progress ?? 0}
-                                color="gray"
-                                w={350}
-                                size="lg"
-                              />
-                              <Text size="sm" fw={500} c="green" w={30}>{sub.progress}%</Text>
+                          {question.sub_questions?.map((sub, subIndex) => (
+                            <Flex key={sub.sub_question_id} align="center" ml="lg" className="group relative w-fit">
+                              <span className="w-3 h-3 border-l-2 border-b-2 border-dotted border-gray-300 mr-2 -translate-y-[2px]" />
+                              <Anchor
+                                underline="hover"
+                                size="sm"
+                                c="dimmed"
+                                onClick={() => router.push(`/instructor/course/${course_id}/process/${assignment_id}/grade-submissions/questions/${question.question_id}/sub-questions/${sub.sub_question_id}/submissions/${sub.submission_id}`)}
+                              >
+                                {index + 1}.{subIndex + 1}: {sub.sub_question_title}
+                              </Anchor>
+                              <Anchor
+                                underline="hover"
+                                size='xs'
+                                ml='xs'
+                                className="invisible group-hover:visible"
+                                onClick={() => router.push(`/instructor/course/${course_id}/process/${assignment_id}/grade-submissions/questions/${question.question_id}/sub-questions/${sub.sub_question_id}/submissions`)}
+                              >
+                                <Flex align="center" gap={4} >
+                                  <VscListUnordered size={12} className='translate-y-[1px]'/>
+                                  <span>Submissions</span>
+                                </Flex>
+                              </Anchor>
                             </Flex>
                           ))}
-                        </Flex>                      
-                      </>
-                    )}
-                  </Table.Td>
+                        </Flex>
+                      )}
+                    </Table.Td>
 
-                  {/* Graded by Column */}
-                  <Table.Td ta="center" className="align-top">
-                    {!hasSub ? (
-                        <Text size="sm" fw={500} c="dimmed">{question.graded_by ?? "-"}</Text>
+                    {/* Points Column */}
+                    <Table.Td ta="center" className="align-top">
+                      <Text mb="xs" size="md" fw={500} h='24.80px' c="dimmed">{question.question_point.toFixed(1)}</Text>
+                      {hasSub && (
+                        <Flex direction="column" gap={16}>
+                          {question.sub_questions?.map((sub) => (
+                            <Text
+                              key={sub.sub_question_id}
+                              size="sm"
+                              fw={500}
+                              c="dimmed"
+                            >
+                              {sub.sub_question_point.toFixed(1)}
+                            </Text>
+                          ))}
+                        </Flex>
+                      )}
+                    </Table.Td>
+
+                    {/* Progress Column */}
+                    <Table.Td ta="center" className="align-top">
+                      {!hasSub ? (
+                        <Flex align="center" justify="center">
+                          <Progress value={question.progress ?? 0} color="green" w={350} size="lg" />
+                          <Text size="sm" fw={500} c="green" w={30}>{question.progress}%</Text>
+                        </Flex>
                       ) : (
                         <>
                           <div className="min-h-[24px] mb-[10px]" />
                           <Flex direction="column" gap={16}>
                             {question.sub_questions?.map((sub) => (
-                              <Text
+                              <Flex
                                 key={sub.sub_question_id}
-                                size="sm"
-                                fw={500}
-                                c="dimmed"
+                                align="center"
+                                justify="center"
+                                gap="2px"
                               >
-                                {sub.graded_by ?? "-"}
-                              </Text>
+                                <Progress
+                                  value={sub.progress ?? 0}
+                                  color="gray"
+                                  w={350}
+                                  size="lg"
+                                />
+                                <Text size="sm" fw={500} c="green" w={30}>{sub.progress}%</Text>
+                              </Flex>
                             ))}
-                          </Flex>
+                          </Flex>                      
                         </>
-                    )}
-                  </Table.Td>
-                </Table.Tr>
-              );
-            })}
-          </Table.Tbody>
-        </Table>
+                      )}
+                    </Table.Td>
+
+                    {/* Graded by Column */}
+                    <Table.Td ta="center" className="align-top">
+                      {!hasSub ? (
+                          <Text size="sm" fw={500} c="dimmed">{question.graded_by ?? "-"}</Text>
+                        ) : (
+                          <>
+                            <div className="min-h-[24px] mb-[10px]" />
+                            <Flex direction="column" gap={16}>
+                              {question.sub_questions?.map((sub) => (
+                                <Text
+                                  key={sub.sub_question_id}
+                                  size="sm"
+                                  fw={500}
+                                  c="dimmed"
+                                >
+                                  {sub.graded_by ?? "-"}
+                                </Text>
+                              ))}
+                            </Flex>
+                          </>
+                      )}
+                    </Table.Td>
+                  </Table.Tr>
+                );
+              })}
+            </Table.Tbody>
+          </Table>
+        </Table.ScrollContainer>
       </Paper>
     </Flex>
   );
