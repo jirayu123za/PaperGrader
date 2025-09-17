cef1f9814c136dbbb52fe349baac23e3049fe879
jirayu <jirayu004za@gmail.com>
2025-09-18 01:40:34 +0700

    feat: rename Published field to PublishedGrade and add PublishedAssignment field for clarity

--- Backend/internal/models/assignment_section.go
+++ Backend/internal/models/assignment_section.go
@@ -11,7 +11,8 @@ type AssignmentSection struct {
 	AssignmentSectionID uuid.UUID  `gorm:"primaryKey" json:"assignment_section_id"`
 	AssignmentID        uuid.UUID  `gorm:"not null" json:"assignment_id"`
 	SectionID           uuid.UUID  `gorm:"not null" json:"section_id"`
-	Published           bool       `gorm:"type:boolean;not null;default:false" json:"published"`
+	PublishedGrade      bool       `gorm:"type:boolean;not null;default:false" json:"published_grade"`
+	PublishedAssignment bool       `gorm:"type:boolean;not null;default:false" json:"published_assignment"`
 	ReleaseDate         *time.Time `gorm:"type:timestamptz" json:"release_date"`
 	DueDate             *time.Time `gorm:"type:timestamptz" json:"due_date"`
 	CutOffDate          *time.Time `gorm:"type:timestamptz" json:"cut_off_date"`
