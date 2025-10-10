02584404dedb841b0d9aaffc67f054c3384348a1
pulom999 <pulom999@gmail.com>
2025-10-11 01:20:56 +0700

    Create useFetchAssigmentStatistic.ts

---
+++ Frontend/hooks/Statistic/useFetchAssigmentStatistic.ts
@@ -0,0 +1,31 @@
+import { useQuery} from '@tanstack/react-query';
+import axios from 'axios';
+
+export interface Assignment {
+  assignment_id: string;
+  assignment_name: string;
+}
+
+
+export function useFetchAssignments(course_id: string | null) {
+  return useQuery<Assignment[], Error>({
+    queryKey: ['exportAssignmentsList', course_id],
+    queryFn: async () => {
+      if (!course_id) throw new Error('Missing course_id');
+      const response = await axios.get<{
+        assignments: Assignment[];
+        message: string;
+      }>(
+        '/api/api/instructor/assignments/export',
+        { params: { course_id } }
+      );
+      if (response.status !== 200) {
+        throw new Error('Network response was not ok');
+      }
+
+      return response.data.assignments;
+    },
+    enabled: Boolean(course_id),
+    refetchOnWindowFocus: false,
+  });
+}
