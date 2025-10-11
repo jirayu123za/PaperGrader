f74b04f1b900fe9a1f16492f1d677d1d72187daa
pulom999 <pulom999@gmail.com>
2025-10-12 00:05:50 +0700

    change hight footer

--- Frontend/app/instructor/course/[course_id]/process/[assignment_id]/review-grade/layout.tsx
+++ Frontend/app/instructor/course/[course_id]/process/[assignment_id]/review-grade/layout.tsx
@@ -4,15 +4,9 @@ import ReviewGradeBottomBar from "@/components/INS/INSProcess/Bottombar/ReviewGr
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative min-h-screen">
-      <main className="pb-20">
-        {children}
-      </main>
-      <footer
-        className="fixed bottom-0 left-64 w-[calc(100%-16rem)] border-t border-gray-300 bg-white z-50"
-        style={{
-          padding: "0.1rem",
-        }}
-      >
+      <main className="pb-9">{children}</main>
+
+      <footer className="fixed bottom-0 left-64 w-[calc(100%-16rem)] h-9 border-t border-gray-300 bg-white z-50 flex items-center pr-4 md:pr-6">
         <ReviewGradeBottomBar />
       </footer>
     </div>
