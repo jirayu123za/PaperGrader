866c5393bb6f51a4e7a09cbd84eed72b4ba7b0e9
pulom999 <pulom999@gmail.com>
2025-10-12 00:06:18 +0700

    change size bottom

--- Frontend/components/INS/INSProcess/Bottombar/ReviewGradeBottomBar.tsx
+++ Frontend/components/INS/INSProcess/Bottombar/ReviewGradeBottomBar.tsx
@@ -1,43 +1,50 @@
 "use client";
-import { Group, Button, Box } from "@mantine/core";
+import { Button, Box } from "@mantine/core";
 import { IconDownload, IconUpload } from "@tabler/icons-react";
 
-export default function BottomBar() {
+export default function ReviewGradeBottomBar() {
   return (
     <Box
-      mt="md"
-      p="sm"
-      style={{
-        display: "flex",
-        justifyContent: "flex-end",
-        gap: "0.5rem",
-      }}
+      p={0}
+      className="w-full h-full flex justify-end items-center gap-1 pl-2 pr-4 md:pr-6"
     >
       <Button
-        leftSection={<IconDownload size={16} />}
+        leftSection={<IconDownload size={12} />}
         variant="default"
         color="indigo"
+        size="xs"
+        radius="sm"
+        className="h-7 px-2"
       >
         Download Grades
       </Button>
       <Button
-        leftSection={<IconDownload size={16} />}
+        leftSection={<IconDownload size={12} />}
         variant="default"
         color="indigo"
+        size="xs"
+        radius="sm"
+        className="h-7 px-2"
       >
         Export Evaluations
       </Button>
       <Button
-        leftSection={<IconDownload size={16} />}
+        leftSection={<IconDownload size={12} />}
         variant="default"
         color="indigo"
+        size="xs"
+        radius="sm"
+        className="h-7 px-2"
       >
         Export Submissions
       </Button>
       <Button
-        leftSection={<IconUpload size={16} />}
+        leftSection={<IconUpload size={12} />}
         variant="filled"
         color="indigo"
+        size="xs"
+        radius="sm"
+        className="h-7 px-2"
       >
         Publish Grades
       </Button>
