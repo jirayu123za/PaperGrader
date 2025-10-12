eb2102d858abcbaf29ead12f569e98f45cd20788
pulom999 <pulom999@gmail.com>
2025-10-12 23:00:08 +0700

    Update layout.tsx

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.css';
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
 import '@mantine/charts/styles.css';
+import '@mantine/dates/styles.css';
 
 import ClientProviders from '@/components/ClientProviders';
 import { Notifications } from '@mantine/notifications';
