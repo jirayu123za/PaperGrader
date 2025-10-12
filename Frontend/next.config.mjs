a354e3dca259d46860b641347e25e3683e818a87
pulom999 <pulom999@gmail.com>
2025-10-13 00:02:51 +0700

    ิีdocker img and compost

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+
   async rewrites() {
     return [
       {
@@ -8,7 +10,24 @@ const nextConfig = {
       },
     ];
   },
+
+
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      canvas: false,
+    };
+    return config;
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
-
