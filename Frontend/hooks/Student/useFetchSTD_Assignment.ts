ebc62e4c7f7170d4125a42a5c7c252afdf2cd97c
Kisaru1523 <76036259+Kisaru1523@users.noreply.github.com>
2025-10-17 12:25:38 +0700

    repairCode

--- Frontend/hooks/Student/useFetchSTD_Assignment.ts
+++ Frontend/hooks/Student/useFetchSTD_Assignment.ts
@@ -12,7 +12,7 @@ interface StudentAssignment {
   assignment_description: string;
   cut_off_date: string;
   due_date: string;
-  release_Date: string;
+  release_date: string;
   section_name: string;
   has_submitted?: boolean; 
 }
