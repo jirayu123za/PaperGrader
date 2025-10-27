a230b71e54de13ff92cc69ea493e79e0b3c5dcb5
pulom999 <pulom999@gmail.com>
2025-10-27 20:52:43 +0700

    add pt overflow

--- Frontend/components/INS/INSProcess/ReviewGrade/ReviewSummary.tsx
+++ Frontend/components/INS/INSProcess/ReviewGrade/ReviewSummary.tsx
@@ -32,7 +32,7 @@ export default function ReviewSummary() {
     noRows;
 
   return (
-    <div className="flex flex-col h-[calc(100vh-80px)] px-4 md:px-6 lg:px-8">
+    <div className="flex flex-col h-[calc(100vh-80px)] px-4 md:px-6 lg:px-8 pt-4 overflow-hidden">
       <Flex
         className="shrink-0 "
         align="center"
