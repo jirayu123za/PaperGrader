c0f7c948ac94ac9a7550b88645d76d94946a3cc5
jirayu <jirayu004za@gmail.com>
2025-10-28 04:34:22 +0700

    Refactor SubmissionGrader component by removing unused RubricGrader import

--- Frontend/components/client/SubmissionGrader.tsx
+++ Frontend/components/client/SubmissionGrader.tsx
@@ -1,13 +1,12 @@
 "use client";
+
 import React from 'react'
-import { RubricGrader } from '@/components/INS/INSProcess/Right/Grade/RubricGrader';
 import GradePdfViewer  from '@/components/INS/GradePdfViewer';
 
 export const SubmissionGrader = () => {
   return (
     <>
-        <GradePdfViewer/>
-        {/* <RubricGrader/> */}
+      <GradePdfViewer/>
     </>
   );
 }
